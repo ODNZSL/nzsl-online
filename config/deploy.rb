a602baf4287b792d560d06c9a7787db44c6a2591
Josh McArthur <joshua.mcarthur@gmail.com>
2011-05-03 17:25:22 +1200

    Added tasks to deploy script and changed cache name for stylesheets
    Former-commit-id: f521b2aa7804c3f87d0e4214ffe44459db5d1365

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "whenever/capistrano"
+
 set :application, "nzsl-online"
 set :repository,  "git@github.com:3months/nzsl-online.git"
 set :deploy_to, "/var/rails/#{application}"
@@ -11,6 +13,8 @@ set :copy_exclude, [".git", "config/database.yml", "config/deploy.rb"]
 set :port, 10
 set :user, "robertja" 
 
+set :whenever_command, "#{try_sudo} whenever --user root"
+
 role :web, "nzsl.vuw.ac.nz"                          # Your HTTP server, Apache/etc
 role :app, "nzsl.vuw.ac.nz"                          # This may be the same as your `Web` server
 role :db,  "nzsl.vuw.ac.nz", :primary => true # This is where Rails migrations will run
@@ -23,7 +27,8 @@ namespace :deploy do
    end
 end
 
-after :update_code do
-  run "#{try_sudo} bundle install --without=development test"
+after "deploy:update_code" do
+  run "cd #{release_path} && ln -s #{shared_path}/system/bundle #{release_path}/vendor/bundle"
+  run "cd #{release_path} && bundle install --deployment --without=development test"
   run "#{try_sudo} ln -s #{shared_path}/system/database.yml #{release_path}/config/database.yml"
 end
