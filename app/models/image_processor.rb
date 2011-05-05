f78aeb74fd8ec74c5e5afaa03f456ff6a12357db
Josh McArthur <joshua.mcarthur@gmail.com>
2011-05-06 10:15:43 +1200

    Adding code to image processor to reset page origin after crop
    Former-commit-id: 067d5ef2ff9777f7e6e7327fe0e626972c2f7a15

--- app/models/image_processor.rb
+++ app/models/image_processor.rb
@@ -5,6 +5,10 @@ class ImageProcessor
   def self.retrieve_and_resize(filename, dimensions = [180, 320])
     image = MiniMagick::Image.open(ImageProcessor.remote_filename(filename))
     image.shave CROP_IMAGES_BY if CROP_IMAGES
+    
+    #Reset page size to cropped image to avoid offset issue.
+    # See here: http://studio.imagemagick.org/pipermail/magick-bugs/2008-May/002933.html
+    image.set("page", "#{image['width']}x#{image['height']}+0+0")    
     image.resize dimensions.join("x") + ">"
     image.format "png"
     image.write ImageProcessor.local_filename(filename, dimensions)
