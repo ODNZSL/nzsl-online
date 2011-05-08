8b0ec4260582a4c688b7e36ce86753f3d39d38a6
Josh McArthur <joshua.mcarthur@gmail.com>
2011-05-09 09:55:52 +1200

    Re-adding task to touch restart.txt
    Former-commit-id: cfa5773a7124e1a0feb8e8a5d586b3db10471517

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ namespace :deploy do
    task :start do ; end
    task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
-     #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
 end
 
