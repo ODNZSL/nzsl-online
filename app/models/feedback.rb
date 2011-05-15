89c8095d6d49e470a944d87ca63a75d19175c580
Josh McArthur <joshua.mcarthur@gmail.com>
2011-05-16 10:48:54 +1200

    Adding email field and validation to feedback model
    Former-commit-id: 3c8896f5aa481d9a545dad2d145775fab67267d3

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,6 +2,8 @@ class Feedback < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :message
   
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :allow_nil => true
+  
   has_attached_file :video,
     :url => "/no-video",
     :path => ":rails_root/data/uploaded/feedback/:id/:basename.:extension"
