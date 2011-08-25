6da0675e39e07bb93520963198aaa9c1cfc3b105
Josh McArthur <joshua.mcarthur@gmail.com>
2011-08-26 10:59:53 +1200

    Added multistage deploy to draft
    Former-commit-id: e65fe5c1dc6d75555e80aadab9cd2a134f19e654

---
+++ config/deploy/production.rb
@@ -0,0 +1,13 @@
+#Redhat Server running at Victoria University of Wellington
+#Technical contact: ITS
+
+set :port, 10
+set :user, "robertja"
+set :branch, "master"
+
+set :whenever_command, "#{try_sudo} whenever --user root"
+
+role :web, "nzsl.vuw.ac.nz"                          # Your HTTP server, Apache/etc
+role :app, "nzsl.vuw.ac.nz"                          # This may be the same as your `Web` server
+role :db,  "nzsl.vuw.ac.nz", :primary => true # This is where Rails migrations will run
+
