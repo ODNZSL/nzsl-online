c06a8982f5ea9c3212c4ab287a9d8acd1988daa8
Mathew Hartley <matt@route66.sytes.net>
2014-01-08 12:19:46 +1300

    cleaning up the asset pipeline for admin layout
    Former-commit-id: 4ecdc98a5ed653b1fee143384d300f29f3eb8278

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,4 @@
+//= require application
+//= require ./admin/ckeditor/ckeditor.js
+//= require ./admin/ckeditor/adapters/jquery.js
+//= require ./admin/admin
--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -3,8 +3,7 @@ $(function(){
   $('ul.record').sortable({containment: 'parent', update: function(event, ui) {
     new_order = [];
     $('ul.record li').each(function() { new_order.push($(this).attr('id').replace('id', '')); });
-    var reorder_path = 
-    reorder_path = window.location.pathname.replace(/((edit|new)\/?)$/, 'page_parts').replace(/(\/)$/,'') + '/reorder';
+    var reorder_path = window.location.pathname.replace(/((edit|new)\/?)$/, 'page_parts').replace(/(\/)$/,'') + '/reorder';
     $.post(reorder_path, {'items[]': new_order});
   }});
 });
