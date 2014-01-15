e341e5f81e3929a5167cb046d800cc7a3b8100b3
Mathew Hartley <matt@route66.sytes.net>
2014-01-15 16:32:04 +1300

    fix page issue, make dir if needed
    Former-commit-id: cead1527728bade09a0eaf9b876e190455bbae34

--- app/models/image_processor.rb
+++ app/models/image_processor.rb
@@ -1,6 +1,7 @@
 class ImageProcessor
   require 'mini_magick'
   require 'open-uri'
+  require 'fileutils'
 
   def self.retrieve_and_resize(filename, dimensions = [180, 320])
     image = MiniMagick::Image.open(ImageProcessor.remote_filename(filename))
@@ -8,7 +9,7 @@ class ImageProcessor
 
     #Reset page size to cropped image to avoid offset issue.
     # See here: http://studio.imagemagick.org/pipermail/magick-bugs/2008-May/002933.html
-    image.set("page", "#{image['width']}x#{image['height']}+0+0")
+    image.set("page", "#{image['width']}X#{image['height']}+0+0")
     image.resize dimensions.join("x") + ">"
     image.format "png"
     image.write ImageProcessor.local_filename(filename, dimensions)
@@ -22,6 +23,7 @@ class ImageProcessor
   def self.create_or_return_path(filename)
     #It is expected that filename is in the 1212/sdsd.png format
     file_parts = filename.split "/"
+    FileUtils.mkdir_p(SIGN_IMAGE_PATH) unless File.exists?(SIGN_IMAGE_PATH)
     if file_parts.length > 1
       return File.join(SIGN_IMAGE_PATH, file_parts[0]) if Dir.exist?(File.join(SIGN_IMAGE_PATH, file_parts[0]))
       Dir.chdir(SIGN_IMAGE_PATH) && Dir.mkdir(file_parts[0])
