f5343e4a1a518aa5a30c28a9b884ec7f542d3bd0
Brenda Wallace <brenda@brenda.nz>
2015-02-03 15:26:28 +1300

    comments update, for rails upgrade, on the test env
    Former-commit-id: 81e5a884a7affe6605868c3638a7aa5a9736467c

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,10 +1,10 @@
 NzslOnline::Application.configure do
-  # Settings specified here will take precedence over those in config/environment.rb
+  # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
-  # test suite.  You never need to work with it otherwise.  Remember that
+  # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs.  Don't rely on the data there!
+  # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
@@ -32,10 +32,8 @@ NzslOnline::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
