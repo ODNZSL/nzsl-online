41a2a8a20f900e54fe365493a354806c959636dc
Rose Lu <roseyylu@gmail.com>
2015-03-27 11:45:43 +1300

    adding in special case for flv
    Former-commit-id: 8e5699c5c3fc78db504b8bc94d8f6cde0455c254

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -73,10 +73,17 @@ $(function(){
         var hidden = wrapper.hasClass('hidden_video');
         wrapper.attr('id', 'video_'+id_offset);
         id_offset += 1;
-        var mp4SourceElement = $('<source />', {src: href + ".mp4"});
-        var webmSourceElement = $('<source />', {src: href + ".webm"});
-        var flvSourceElement = $('<source />', {src: href + ".flv"});
-        var videoElement  = $('<video />', {controls: "controls", preload: !hidden, loop: wrapper.data('loop')}).append(webmSourceElement, mp4SourceElement, flvSourceElement);
+        var videoElement;
+        if (href.includes(".flv")) {
+          // special case for flv videos
+          var flvSourceElement = $('<source />', {src: href});
+          videoElement  = $('<video />', {controls: "controls", preload: !hidden, loop: wrapper.data('loop')}).append(flvSourceElement);
+        }
+        else {
+          var mp4SourceElement = $('<source />', {src: href + ".mp4"});
+          var webmSourceElement = $('<source />', {src: href + ".webm"});
+          videoElement  = $('<video />', {controls: "controls", preload: !hidden, loop: wrapper.data('loop')}).append(webmSourceElement, mp4SourceElement);
+        }
         wrapper.empty().append(videoElement);
       });
     }
