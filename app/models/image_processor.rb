5f0186d58bd499422057aea37f15a735760f8de3
Brenda Wallace <brenda@rabidtech.co.nz>
2015-09-10 01:44:46 +0000

    comments and style fixups for ImageProcessor
    Former-commit-id: 786e77311abf740d51643f4a7702b81365638c34

--- app/models/image_processor.rb
+++ app/models/image_processor.rb
@@ -1,10 +1,11 @@
+## Retrieves images from Freelex, resizes and saves locally
 class ImageProcessor
   require 'mini_magick'
   require 'open-uri'
   require 'fileutils'
 
   def self.retrieve_and_resize(filename, dimensions = [180, 320])
-    puts "Fetching " + filename
+    puts 'Fetching ' + filename
     remote_filename = ImageProcessor.remote_filename(filename)
     begin
       image = MiniMagick::Image.open(remote_filename)
@@ -14,26 +15,26 @@ class ImageProcessor
       puts "perhaps image magick isn't installed, or is not on the PATH"
       raise
     end
- 
-    #Reset page size to cropped image to avoid offset issue.
+
+    # Reset page size to cropped image to avoid offset issue.
     # See here: http://studio.imagemagick.org/pipermail/magick-bugs/2008-May/002933.html
     width  = image['width']
     height = image['height']
-    image.set("page", "#{width}x#{height}+0+0")
-    image.resize dimensions.join("x") + ">"
-    image.format "png"
+    image.set('page', "#{width}x#{height}+0+0")
+    image.resize dimensions.join('x') + '>'
+    image.format 'png'
     image.write ImageProcessor.local_filename(filename, dimensions)
-    return ImageProcessor.local_filename(filename, dimensions)
+    ImageProcessor.local_filename(filename, dimensions)
   end
 
-  def self.remote_filename(filename = "")
+  def self.remote_filename(filename = '')
     ASSET_URL + filename
   end
 
   def self.create_or_return_path(filename)
-    #It is expected that filename is in the 1212/sdsd.png format
-    file_parts = filename.split "/"
-    FileUtils.mkdir_p(SIGN_IMAGE_PATH) unless File.exists?(SIGN_IMAGE_PATH)
+    # It is expected that filename is in the 1212/sdsd.png format
+    file_parts = filename.split '/'
+    FileUtils.mkdir_p(SIGN_IMAGE_PATH) unless File.exist?(SIGN_IMAGE_PATH)
     if file_parts.length > 1
       return File.join(SIGN_IMAGE_PATH, file_parts[0]) if Dir.exist?(File.join(SIGN_IMAGE_PATH, file_parts[0]))
       Dir.chdir(SIGN_IMAGE_PATH) && Dir.mkdir(file_parts[0])
@@ -43,8 +44,8 @@ class ImageProcessor
     end
   end
 
-  def self.local_filename(filename = "", dimensions = [180, 320])
-    File.join(ImageProcessor.create_or_return_path(filename), dimensions.join("x") + "-#{filename.gsub(/[\/\\]/, "-")}")
+  def self.local_filename(filename = '', dimensions = [180, 320])
+    File.join(ImageProcessor.create_or_return_path(filename),
+              dimensions.join('x') + "-#{filename.gsub(/[\/\\]/, "-")}")
   end
-
 end
