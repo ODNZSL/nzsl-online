79d8fca8fc078aa4241399d789db61c37c33df47
Brenda Wallace <brenda@rabidtech.co.nz>
2015-09-10 04:56:19 +0000

    code style fixes in sign spec
    Former-commit-id: 5ff89b2761ef03027f3ea721fef4ab22df38a74a

--- spec/models/sign_spec.rb
+++ spec/models/sign_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe "Sign" do
-
+describe 'Sign' do
   it 'must create a new Sign object' do
     sign = Sign.new
     expect(sign.is_a?(Sign)).to eq(true)
@@ -10,7 +9,7 @@ describe "Sign" do
   it 'saves a new sign' do
     sign = Sign.new
     expect(sign.is_a?(Sign)).to eq(true)
-    #TODO test passing data into initalizing
+    # TODO: test passing data into initalizing
   end
 
   it 'retrieves two random signs' do
@@ -19,7 +18,6 @@ describe "Sign" do
     expect(signOne.is_a?(Sign)).to eq(true)
     expect(signTwo.is_a?(Sign)).to eq(true)
     expect(signOne.id).not_to eq(signTwo.id)
-
   end
 
   it 'finds a sign' do
@@ -28,58 +26,53 @@ describe "Sign" do
   end
 
   it 'must have set class attributes' do
-    
     expect(SIGN_URL.match(/\Ahttp\:/).is_a?(MatchData)).to eq(true)
     expect(ASSET_URL.match(/\Ahttp\:/).is_a?(MatchData)).to eq(true)
   end
 
   it 'must have all attributes of a Sign' do
-    #As per the spec'd requirements of DNZSL:
+    # As per the spec'd requirements of DNZSL:
     required_attributes = [
-       :id,
-       :gloss_main,
-       :gloss_secondary,
-       :gloss_minor,
-       :gloss_maori,
-       :word_classes,
-       :inflection,
-       :age_groups,
-       :gender_groups,
-       :video,
-       :drawing,
-       :usage_notes,
-       :contains_numbers,
-       :is_fingerspelling,
-       :is_directional,
-       :is_locatable
+      :id,
+      :gloss_main,
+      :gloss_secondary,
+      :gloss_minor,
+      :gloss_maori,
+      :word_classes,
+      :inflection,
+      :age_groups,
+      :gender_groups,
+      :video,
+      :drawing,
+      :usage_notes,
+      :contains_numbers,
+      :is_fingerspelling,
+      :is_directional,
+      :is_locatable
     ]
     sign = Sign.new
     required_attributes.each do |att|
       expect(sign.respond_to?(att)).to eq(true)
-      expect(sign.respond_to?(att.to_s + "=", "")).to eq(true)
+      expect(sign.respond_to?(att.to_s + '=', '')).to eq(true)
     end
   end
 
-
   it 'must find a single Sign with a simple search string' do
-    #TODO fix this test
+    # TODO: fix this test
     # params = {
-      
+
     # }
     # search_result = Sign.search(params)
     # puts search_result
     # expect(search_result).to eq(true)
   end
 
-
   it 'must not throw errors' do
-    #call the classmethods
+    # call the classmethods
     Sign.usage_tags
     Sign.topic_tags
     Sign.locations
     Sign.location_groups
-    #TODO check returned result
+    # TODO: check returned result
   end
-
 end
-
