eba031b23f4895975a2d18b7e5b2f7b72dc19c92
Brenda Wallace <brenda@rabidtech.co.nz>
2015-09-10 04:05:15 +0000

    code style fixes for app/models/setting.rb
    Former-commit-id: 3bd03813ca6fd4c40257a6d8a6fb6992f127a8b4

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,14 +1,13 @@
+## Settings for the web app
 class Setting < ActiveRecord::Base
-  
   def self.update_all(params)
-    params.each do |k,v| 
+    params.each do |k, v|
       Setting.where(key: k).first_or_create.update_attributes(value: v)
     end
   end
-  
+
   def self.get(key)
     setting = find_by_key(key.to_s)
     setting.value if setting
   end
-
 end
