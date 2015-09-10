bec1c6a885bb9aa19bc71f0cd42ffdfdf32dc400
Brenda Wallace <brenda@rabidtech.co.nz>
2015-09-10 04:37:27 +0000

    removed hard coded path in spec
    Former-commit-id: 52a8866d7c232b20e6dbf16bac5f361e5975417c

--- spec/models/image_processor_spec.rb
+++ spec/models/image_processor_spec.rb
@@ -24,11 +24,11 @@ describe 'ImageProcessor' do
     expect(actual_height).to be <= height
   end
   it 'works out the remote path to image' do
-
     expect(ImageProcessor.remote_filename(filename)).to start_with('http://')
   end
   it 'works out the local path to image' do
-    expect(ImageProcessor.local_filename(filename)).to eq('/home/brenda/projects/nzsl/public/system/images/signs/1935/180x320-1935-picture-W99-69.png')
+    expect(ImageProcessor.local_filename(filename)).to end_with(
+      '/system/images/signs/1935/180x320-1935-picture-W99-69.png')
   end
   it 'create_or_return_path' do
 #    ImageProcessor.create_or_return_path(filename)
