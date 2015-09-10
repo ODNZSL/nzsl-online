41b460319a34d82309f50d87a441f8d16f9e6ebd
Brenda Wallace <brenda@rabidtech.co.nz>
2015-09-10 03:53:02 +0000

    Ruby 2 style updates
    Former-commit-id: 21e1bd0db8a64b0034858f78d886db2c8646f69c

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,18 +4,20 @@ class Feedback < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :message
 
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :allow_nil => true
+  validates_format_of :email,
+                      with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                      allow_nil: true
 
   has_attached_file :video,
-    :url => '/no-video',
-    :path => ':rails_root/data/uploaded/feedback/:id/:basename.:extension'
+                    url: '/no-video',
+                    path: ':rails_root/data/uploaded/feedback/:id/:basename.:extension'
 
-  validates_attachment_size :video, :less_than => 50.megabytes, :allow_nil => true
+  validates_attachment_size :video, less_than: 50.megabytes, allow_nil: true
 
   def send_email
     FeedbackMailer.email(self).deliver
     self.video = nil
-    self.save
+    save
   rescue
     return false
   end
