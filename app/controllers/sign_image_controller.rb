7a0c4bc6d33136747ce288aa71d4300c67824036
Brenda Wallace <brenda@rabidtech.co.nz>
2015-09-18 14:46:16 +1200

    simplified SignImageController#show
    and stopped processing on 404
    
    
    Former-commit-id: 0f2220e0e883aeb7bb4b85a02c4cc2c1ec97ade9

--- app/controllers/sign_image_controller.rb
+++ app/controllers/sign_image_controller.rb
@@ -1,20 +1,16 @@
 class SignImageController < ApplicationController
   def show
-    if File.exist?(
-      ImageProcessor.local_filename(params[:filename], [params[:width].to_i, params[:height].to_i])
-    )
-      send_file(ImageProcessor.local_filename(
+    image_filename = ImageProcessor.local_filename(params[:filename], [params[:width].to_i, params[:height].to_i])
+
+    return send_file(image_filename, type: 'image/png', disposition: 'attachment', filename: params[:filename]) if File.exist?(image_filename)
+
+    begin
+      send_file(ImageProcessor.retrieve_and_resize(
                   params[:filename],
                   [params[:width], params[:height]]), type: 'image/png', disposition: 'attachment', filename: params[:filename])
-    else
-      begin
-        send_file(ImageProcessor.retrieve_and_resize(
-                    params[:filename],
-                    [params[:width], params[:height]]), type: 'image/png', disposition: 'attachment', filename: params[:filename])
-      rescue Exception => e
-        logger.error e
-        render nothing: true, status: 404
-      end
+    rescue Exception => e
+      logger.error e
+      render nothing: true, status: 404
     end
   end
 end
