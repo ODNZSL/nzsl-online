869cee37cf4350c70681cc1bd82a4fa77b482e42
Brenda Wallace <brenda@rabidtech.co.nz>
2015-10-07 14:16:36 +1300

    split parsing into smaller methods
    Former-commit-id: 55ae172a87c607e9d3584828263ab4f3676b1230

--- app/models/sign_parser.rb
+++ app/models/sign_parser.rb
@@ -9,28 +9,49 @@ class SignParser
     return sign if @data.nil?
 
     @sign.id = @data.value_for_tag('headwordid')
-    @sign.video = "#{ASSET_URL}#{@data.value_for_tag('ASSET glossmain')}"
 
+    parse_gloss
+    parse_video
+    parse_images
+    parse_grammar
+    parse_extras
+    parse_examples
+
+    @sign
+  end
+
+  def parse_gloss
     # gloss
     @sign.gloss_main = @data.value_for_tag('glossmain')
     @sign.gloss_secondary = @data.value_for_tag('glosssecondary')
     @sign.gloss_minor = @data.value_for_tag('glossminor')
     @sign.gloss_maori = @data.value_for_tag('glossmaori')
+  end
 
+  def parse_video
+    @sign.video = "#{ASSET_URL}#{@data.value_for_tag('ASSET glossmain')}"
     @sign.video_slow = "#{ASSET_URL}#{@data.value_for_tag('ASSET glossmain_slow')}" if @data.value_for_tag('ASSET glossmain_slow').present?
+  end
+
+  def parse_images
     @sign.drawing = @data.value_for_tag('ASSET picture')
     @sign.handshape = @data.value_for_tag('handshape')
     @sign.location_name = @data.value_for_tag('location')
+  end
 
+  def parse_grammar
     # grammar
     @sign.word_classes = @data.value_for_tag('SECONDARYWORDCLASS')
     @sign.inflection = @data.value_for_tag('INFLECTION')
+
     @sign.contains_numbers = @data.value_for_tag('number_incorp').to_bool
     @sign.is_fingerspelling = @data.value_for_tag('fingerspelling').to_bool
     @sign.is_directional = @data.value_for_tag('directional').to_bool
     @sign.is_locatable = @data.value_for_tag('locatable').to_bool
     @sign.one_or_two_handed = @data.value_for_tag('one_or_two_hand').to_bool
+  end
 
+  def parse_extras
     # notes
     @sign.age_groups = @data.value_for_tag('VARIATIONAGE')
     @sign.gender_groups = @data.value_for_tag('VARIATIONGENDER')
@@ -38,7 +59,9 @@ class SignParser
     @sign.usage = @data.value_for_tag('usage')
     @sign.usage_notes = @data.value_for_tag('essay')
     @sign.related_to = @data.value_for_tag('RELATEDTO')
+  end
 
+  def parse_examples
     # examples
     @sign.examples = []
     VIDEO_EXAMPLES_TOTAL.times do |i|
@@ -49,8 +72,6 @@ class SignParser
                          video_slow: (@data.value_for_tag("ASSET finalexample#{i}_slow").present? ? "#{ASSET_URL}#{@data.value_for_tag("ASSET finalexample#{i}_slow")}" : nil)
                        }
     end
-
-    @sign
   end
 
   def parse_transcription(data, tag)
