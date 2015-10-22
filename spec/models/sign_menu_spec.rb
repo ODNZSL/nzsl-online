5766ac863987270c41e39be2906e496eacad6389
Brenda Wallace <brenda@rabidtech.co.nz>
2015-10-23 11:30:05 +1300

    moved SignMenu specs to own file
    Former-commit-id: 721fc92cc9a34d167403726af9cd1dc34684b4e1

---
+++ spec/models/sign_menu_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'SignMenu' do
+  it 'must not throw errors' do
+    # call the classmethods
+    SignMenu.usage_tags
+    SignMenu.topic_tags
+    SignMenu.locations
+    SignMenu.location_groups
+    # TODO: check returned result
+  end
+end
