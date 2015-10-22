c05ef9b6bf08ab66ac6cfb303361c3f93c6889f3
Brenda Wallace <brenda@rabidtech.co.nz>
2015-10-22 17:18:23 +1300

    nested class
    Conflicts:
    	app/controllers/admin/page_parts_controller.rb
    
    Former-commit-id: 859f66bd0e58c3eee1f0bcbb12b3cd2289bdef60

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,28 +1,30 @@
-class Admin::SettingsController < ApplicationController
-  layout 'admin'
-  before_action :authenticate, :set_title
+class Admin
+  class SettingsController < ApplicationController
+    layout 'admin'
+    before_action :authenticate, :set_title
 
-  def show
-    redirect_to edit_admin_settings_path
-  end
-
-  def edit
-  end
-
-  def update
-    if Setting.update_all(params[:settings])
-      flash[:notice] = 'Settings were successfully saved.'
+    def show
+      redirect_to edit_admin_settings_path
     end
-    render :edit
-  end
 
-  private
+    def edit
+    end
 
-  def set_title
-    @title = 'Administrate Settings'
-  end
+    def update
+      if Setting.update_all(params[:settings])
+        flash[:notice] = 'Settings were successfully saved.'
+      end
+      render :edit
+    end
 
-  def settings_params
-    params.require(:settings).permit(:key, :value)
+    private
+
+    def set_title
+      @title = 'Administrate Settings'
+    end
+
+    def settings_params
+      params.require(:settings).permit(:key, :value)
+    end
   end
 end
