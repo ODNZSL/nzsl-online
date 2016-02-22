ad47f20a745bbcd8e5e9376ed0d99abc6a34d2f7
Brenda Wallace <brenda@rabidtech.co.nz>
2016-02-22 21:13:51 +0000

    digest auth helper
    Former-commit-id: ff3d4c6962d040a9243694f18a803d46e9830572

--- spec/controllers/admin/settings_controller_spec.rb
+++ spec/controllers/admin/settings_controller_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Admin::SettingsController, type: :controller do
-  describe '#show' do
-    context "not logged in" do
+  context 'Not logged in' do
+    describe '#show' do
       before { get :show }
       it { expect(response).to have_http_status(:unauthorized) }
     end
-  end
 
-  describe '#edit' do
-    context "not logged in" do
+    describe '#edit' do
       before { get :edit }
       it { expect(response).to have_http_status(:unauthorized) }
     end
-  end
 
-  describe '#update' do
-    context "not logged in" do
+    describe '#update' do
       before { patch :update }
       it { expect(response).to have_http_status(:unauthorized) }
     end
+  end
+
+  context 'when HTTP Digest auth credentials are invalid' do
+    before do
+      authenticate_with_http_digest('invalid_login', 'invalid_password') do
+        get :show
+      end
+    end
+    it { expect(response).to have_http_status(:unauthorized) }
   end
 end
