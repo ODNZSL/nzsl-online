9805d51c7837b74e10b561e9dc201f0b0f9a4932
Brenda Wallace <brenda@rabidtech.co.nz>
2016-02-23 23:11:43 +0000

    more specs for admin/pages
    Former-commit-id: 64677e328aee5ee1963c3131fa45d5da43cee829

--- spec/controllers/admin/pages_controller_spec.rb
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -1,50 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe Admin::SettingsController, type: :controller do
-  let(:username) { 'test'}
+RSpec.describe Admin::PagesController, type: :controller do
+  let(:username) { 'test' }
   let(:password) { 'test' }
-  before do
-    NZSL_ADMIN_ACCESS[username] = Digest::SHA1.hexdigest(password)
+
+  let(:page) { FactoryGirl.create(:page) }
+  let(:valid_page_params) do
+    { title: "updated page title" }
   end
-  context "when HTTP auth credentials are good" do
-    before do
-      basic_auth username, password
-    end
-    describe 'GET #show' do
-      before do
-        get :show
-      end
-      it { expect(response).to have_http_status(:redirect) }
+
+  before { NZSL_ADMIN_ACCESS[username] = Digest::SHA1.hexdigest(password) }
+
+  context 'when HTTP auth credentials are good' do
+    before { basic_auth username, password }
+
+    describe 'GET #index' do
+      before { get :index }
+      it { expect(response).to have_http_status(:success) }
+      it { expect(response).to render_template(:index) }
     end
 
     describe 'GET #edit' do
-      before do
-        get :edit
-      end
+      before { get :edit, id: page.to_param }
       it { expect(response).to have_http_status(:success) }
       it { expect(response).to render_template(:edit) }
     end
   end
 
-
   context 'Not logged in' do
     describe '#index' do
-      before { get :show }
+      before { get :index }
       it { expect(response).to have_http_status(:unauthorized) }
     end
 
-    describe '#show' do
-      before { get :show }
+    describe '#new' do
+      before { get :new }
       it { expect(response).to have_http_status(:unauthorized) }
     end
 
     describe '#edit' do
-      before { get :edit }
+      before { get :edit, id: page.to_param }
       it { expect(response).to have_http_status(:unauthorized) }
     end
 
     describe '#update' do
-      before { patch :update }
+      before { patch :update, id: page.to_param, page: valid_page_params }
       it { expect(response).to have_http_status(:unauthorized) }
     end
   end
@@ -52,7 +52,7 @@ RSpec.describe Admin::SettingsController, type: :controller do
   context 'when HTTP Digest auth credentials are invalid' do
     before do
       basic_auth 'alice', 'secret'
-      get :show
+      get :index
     end
     it { expect(response).to have_http_status(:unauthorized) }
   end
