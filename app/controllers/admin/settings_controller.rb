9993e3ac7d6930a48cf793fc25a0e4cac4fba514
Brenda Wallace <brenda@rabidtech.co.nz>
2016-02-29 02:10:39 +0000

    changed settings#update to use stronger params
    Former-commit-id: 858ff8ee2b1b16014b768d5ec65d5c03da2166b9

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,28 +1,30 @@
-class Admin::SettingsController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
-  layout 'admin'
-  before_action :authenticate, :set_title
+module Admin
+  class SettingsController < ApplicationController
+    layout 'admin'
+    before_action :authenticate, :set_title
 
-  def show
-    redirect_to edit_admin_settings_path
-  end
-
-  def edit
-  end
-
-  def update
-    if Setting.update_all(params[:settings])
-      flash[:notice] = 'Settings were successfully saved.'
+    def show
+      redirect_to edit_admin_settings_path
     end
-    redirect_to edit_admin_settings_path
-  end
 
-  private
+    def edit
+    end
 
-  def set_title
-    @title = 'Administrate Settings'
-  end
+    def update
+      if Setting.update_all(settings_params)
+        flash[:notice] = 'Settings were successfully saved.'
+      end
+      redirect_to edit_admin_settings_path
+    end
 
-  def settings_params
-    params.require(:settings).permit(:key, :value)
+    private
+
+    def set_title
+      @title = 'Administrate Settings'
+    end
+
+    def settings_params
+      params.require(:settings)
+    end
   end
 end
