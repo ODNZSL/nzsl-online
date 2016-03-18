60d11e6714cff6822569b1329323288232995dc8
Brenda Wallace <brenda@rabidtech.co.nz>
2016-03-18 01:38:13 +0000

    vocab sheets controller tests
    Former-commit-id: 371c22b31a49327a07cde5280759c5f803f0ac90

--- spec/controllers/vocab_sheets_controller_spec.rb
+++ spec/controllers/vocab_sheets_controller_spec.rb
@@ -1,25 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe VocabSheetsController, type: :controller do
-  let(:valid_params) { FactoryGirl.attributes_for(:vocab_sheet ) }
-  let(:vocab_sheet) { FactoryGirl.create(:vocab_sheet)}
+  let(:valid_params) { FactoryGirl.attributes_for(:vocab_sheet) }
+  let(:vocab_sheet) { FactoryGirl.create(:vocab_sheet) }
+  let(:session) { { vocab_sheet_id: vocab_sheet.id } }
 
-  describe '#show' do
+  let(:new_name) { 'this is the new name' }
+  let(:new_attributes) { { vocab_sheet: { name: new_name } } }
+
+  context '#show' do
     before { get :show }
     it { expect(response).to have_http_status(:ok) }
   end
 
-  describe '#update' do
-    it 'updates an vocab sheet' do
-      patch :update, vocab_sheet: valid_params
-      expect(response).to have_http_status(:found)
+  context '#update' do
+    describe 'new vocab sheet' do
+      before { patch :update, new_attributes }
+      it { expect(assigns(:sheet)) }
+      it { expect(response).to redirect_to root_path }
+    end
+
+    describe 'updating existing vocab sheet' do
+      before { patch :update, new_attributes, session }
+      it { expect(assigns(:sheet)).to eq(vocab_sheet) }
+      it 'updates the sheet name' do
+        vocab_sheet.reload
+        expect(vocab_sheet.name).to eq(new_name)
+      end
+      it { expect(response).to redirect_to root_path }
     end
   end
-  describe '#destroy' do
-    before { vocab_sheet.save! }
-    it 'destroys an item' do
-      delete :destroy, id: vocab_sheet.id
-      expect(response).to have_http_status(:found)
-    end
+
+  context '#destroy' do
+    before { delete :destroy, id: vocab_sheet.id }
+    it { expect(response).to redirect_to root_path }
   end
 end
