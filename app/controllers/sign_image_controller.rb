5e6b36aed68dee2358e2040657c42fe75039355e
Brenda Wallace <brenda@rabidtech.co.nz>
2016-03-21 15:27:32 +1300

    moved param checking into own methods
    Former-commit-id: d6d74a7a6a3ced0a698b942390e9622d91b0d968

--- app/controllers/sign_image_controller.rb
+++ app/controllers/sign_image_controller.rb
@@ -1,12 +1,35 @@
 class SignImageController < ApplicationController
   def show
-    local_filename = ImageProcessor.new(filename: params['filename'],
-                                        height: params['height'],
-                                        width: params['width']).resize_and_cache
+    @local_filename = ImageProcessor.new(filename: filename_param,
+                                         height: height_param,
+                                         width: width_param).resize_and_cache
 
-    send_file(local_filename,
+    send_file(@local_filename,
               type: 'image/png',
               disposition: 'attachment',
-              filename: params[:filename])
+              filename: filename_param)
+  end
+
+  private
+
+  def filename_param
+    sign_image_params[:filename] if %r{^[\d]+\/[a-zA-z\-0-9]+\.png$}.match(sign_image_params[:filename])
+    fail 'Invalid filename'
+  end
+
+  def width_param
+    Integer(sign_image_params[:width])
+  rescue
+    100
+  end
+
+  def height_param
+    Integer(sign_image_params[:height])
+  rescue
+    100
+  end
+
+  def sign_image_params
+    params.permit(:filename, :height, :width)
   end
 end
