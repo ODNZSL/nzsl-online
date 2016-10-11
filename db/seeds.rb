9ba6f9d0a7502e811197dec5c57c0eb1c5d7128b
Brenda Wallace <brenda@wallace.net.nz>
2016-10-12 10:59:04 +1300

    Don't glob for csv
    Former-commit-id: 609b98b4b5a8144da3c7faf2ee3d7deaa922750f

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,11 @@ require 'csv'
 def load_pages
   Page.transaction do
     source_path = Rails.root.join('db', 'seeds')
-    Dir.glob("#{source_path}/pages.csv").each do |pages_file|
-      Rails.logger.info "Loading pages from #{pages_file}..."
-      CSV.foreach(pages_file) do |row|
-        page = Page.create_from_csv(row)
-        Rails.logger.info "\tCreated #{page.slug} page"
-      end
+    pages_file = "#{source_path}/pages.csv"
+    Rails.logger.info "Loading pages from #{pages_file}..."
+    CSV.foreach(pages_file) do |row|
+      page = Page.create_from_csv(row)
+      Rails.logger.info "\tCreated #{page.slug} page"
     end
   end
   Rails.logger.info 'Finished loading pages'
@@ -16,12 +15,11 @@ end
 def load_page_parts
   PagePart.transaction do
     source_path = Rails.root.join('db', 'seeds')
-    Dir.glob("#{source_path}/page_parts.csv").each do |page_parts_file|
-      Rails.logger.info "Loading page parts from #{page_parts_file}..."
-      CSV.foreach(page_parts_file) do |row|
-        page_part = PagePart.create_from_csv(row)
-        Rails.logger.info "\tCreated #{page_part.slug} page_part"
-      end
+    page_parts_file = "#{source_path}/page_parts.csv"
+    Rails.logger.info "Loading page parts from #{page_parts_file}..."
+    CSV.foreach(page_parts_file) do |row|
+      page_part = PagePart.create_from_csv(row)
+      Rails.logger.info "\tCreated #{page_part.slug} page_part"
     end
   end
   Rails.logger.info 'Finished loading page parts'
@@ -30,12 +28,11 @@ end
 def load_settings
   Setting.transaction do
     source_path = Rails.root.join('db', 'seeds')
-    Dir.glob("#{source_path}/settings.csv").each do |settings_file|
-      Rails.logger.info "Loading page parts from #{settings_file}..."
-      CSV.foreach(settings_file) do |row|
-        setting = Setting.create_from_csv(row)
-        Rails.logger.info "\tCreated #{setting.key} setting"
-      end
+    settings_file = "#{source_path}/settings.csv"
+    Rails.logger.info "Loading settings from #{settings_file}..."
+    CSV.foreach(settings_file) do |row|
+      setting = Setting.create_from_csv(row)
+      Rails.logger.info "\tCreated #{setting.key} setting"
     end
   end
   Rails.logger.info 'Finished loading settings'
