1842060f6765cda643519eaf280e94be55a74279
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-05-18 10:52:36 +1200

    Added necessary scss components.
    Former-commit-id: dbe9243c272bd8f7dbb76209f34f9340e18e5f26

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,7 +1,13 @@
+// $(document).on("click", ".hamburger-menu-icon", function(){
+//   $("#toggle-menu").show();
+// });
+
+/* Open when someone clicks on the span element */
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 
+/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
