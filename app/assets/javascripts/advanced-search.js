5a0746f6bef05079ec21b679d11f7de467ad0b66
KatieWright26 <katie.wrght@gmail.com>
2017-05-17 16:31:50 +1200

    prevent default on click
    Former-commit-id: a9216b5b13d547823b696b54d1972b6d9262f469

--- app/assets/javascripts/advanced-search.js
+++ app/assets/javascripts/advanced-search.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   var windowWidth = $(window).width();
   $('.advanced a').click(function(e) {
+    e.preventDefault();
     if(windowWidth >= 640) {
-      e.preventDefault();
       $('.advanced-search-container').removeClass('hide');
       $('.advanced-search-container').addClass('show')
     } else {
