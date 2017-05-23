4cc34ad20f6ac0e0fb6191748fb2db5bcb511c5f
KatieWright26 <katie.wrght@gmail.com>
2017-05-24 10:52:18 +1200

    make color stick to list item being searched
    Former-commit-id: 906a18474c590280d869b255e16c620d544f3d4e

--- app/assets/javascripts/select_menu_search.js
+++ app/assets/javascripts/select_menu_search.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
+
   $('.usage-dropdown').click(function(e) {
     e.preventDefault;
     var id = $(this).attr('id');
+    removeStyle($('.usage-dropdown'))
+    addStyle($(this));
     $('#usage').val(id)
   })
+
   $('.topic-dropdown').click(function(e) {
     e.preventDefault;
     var id = $(this).attr('id');
+    removeStyle($('.topic-dropdown'))
+    addStyle($(this));
     $('#tag').val(id)
   })
+
+  function addStyle(item) {
+    item.css("background-color", "#F0F6FA")
+           .addClass('selected');
+  }
+
+  function removeStyle(className) {
+    className.removeClass('selected')
+             .css("background-color", "");
+  }
 })
