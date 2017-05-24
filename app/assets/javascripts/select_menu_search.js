8b61ae60ae544fb7cb3a9198aa477f99b84a07f9
KatieWright26 <katie.wrght@gmail.com>
2017-05-24 12:34:34 +1200

    clear option wasn't appearing when topic/usage selected. Fixed
    Former-commit-id: ed4c55813f12ef63c455804e44bc8d89c038c378

--- app/assets/javascripts/select_menu_search.js
+++ app/assets/javascripts/select_menu_search.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     var id = $(this).attr('id');
     removeStyle($('.usage-dropdown'))
     addStyle($(this));
+    clearDropdown($('.usage-dropdown'));
     $('#usage').val(id)
   })
 
@@ -13,16 +14,24 @@ $(document).ready(function() {
     var id = $(this).attr('id');
     removeStyle($('.topic-dropdown'))
     addStyle($(this));
+    clearDropdown($('.topic-dropdown'));
     $('#tag').val(id)
   })
 
-  function addStyle(item) {
-    item.css("background-color", "#F0F6FA")
+  function addStyle(listItem) {
+    listItem.css("background-color", "#F0F6FA")
            .addClass('selected');
   }
 
-  function removeStyle(className) {
-    className.removeClass('selected')
+  function removeStyle(list) {
+    list.removeClass('selected')
              .css("background-color", "");
+  }
+
+  function clearDropdown(list) {
+    $('.empty').css("display", "block")
+    $('.empty').click(function() {
+      list.css("background-color");
+    })
   }
 })
