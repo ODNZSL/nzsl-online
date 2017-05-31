a2f442723a4cd5f9c5c1229118471a24abf2f2c7
elspeththewitch2 <elspeth@rabidtech.co.nz>
2017-05-31 15:18:29 +1200

    Fixd the play pause video flow
    Former-commit-id: 45b9805dfdf90b635891721eeeb2633534724228

--- app/assets/javascripts/custom_play_button.js
+++ app/assets/javascripts/custom_play_button.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
-  $('video').click(function(e) {
+  $("video").click(function(e) {
     e.preventDefault();
-    this.paused == true ? playVideo($(this)):pauseVideo($(this));
+    this.paused == true ? playVideo(this) : pauseVideo(this);
   });
 
   function playVideo(container) {
-    container.closest('.video-container').children('.play-button').css("display", "none");
-    container.get(0).play();
+    pauseOtherVideos(container);
+    $(container).closest(".video-container").children(".play-button").css("display", "none");
+    $(container).get(0).play();
   }
 
   function pauseVideo(container) {
-    container.closest('.video-container').children('.play-button').css("display", "inline-block");
-    container.get(0).pause();
+    $(container).closest(".video-container").children(".play-button").css("display", "inline-block");
+    $(container).get(0).pause();
   }
 
-  $(document).click(function(e) {
-    if(!$(e.target).hasClass('video')) {
-      $(this).find('video').each(function() {
-        pauseVideo($(this))
-      });
-    }
-  })
+  function pauseOtherVideos(currentVideo) {
+    $("video").each(function() {
+      if (this != currentVideo) { pauseVideo(this); }
+    });
+  }
 });
