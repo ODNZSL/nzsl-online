0f693011b805eee9fd7adff94902eae8276f5c61
elspeththewitch2 <elspeth@rabidtech.co.nz>
2017-05-31 17:49:45 +1200

    Modernizr to the rescue
    Former-commit-id: 0e0a47c8f4219fd8101665572b7edf664cf4d1bd

--- app/assets/javascripts/custom_play_button.js
+++ app/assets/javascripts/custom_play_button.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
-  $('video').click(function(e) {
+  if(Modernizr.touch) {
+    $(".play-button").hide();
+    $("video").each(function() { $(this).prop("controls", true); })
+  }
+
+  $("video").click(function(e) {
     e.preventDefault();
     this.paused == true ? playVideo($(this)):pauseVideo($(this));
   });
 
   function playVideo(container) {
-    container.closest('.video-container').children('.play-button').css("display", "none");
+    container.closest(".video-container").children(".play-button").css("visibility", "hidden");
     container.get(0).play();
   }
 
   function pauseVideo(container) {
-    container.closest('.video-container').children('.play-button').css("display", "inline-block");
+    container.closest(".video-container").children(".play-button").css("visibility", "visible");
     container.get(0).pause();
   }
 
   $(document).click(function(e) {
-    if(!$(e.target).hasClass('video')) {
-      $(this).find('video').each(function() {
+    if(!$(e.target).hasClass("video")) {
+      $(this).find("video").each(function() {
         pauseVideo($(this))
       });
     }
