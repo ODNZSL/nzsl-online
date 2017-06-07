c973275315dfc5cdbd076521bb80f1e87d8ac563
Brenda Wallace <brenda@wallace.net.nz>
2017-06-07 02:29:36 +0000

    Update smtp domain to herokuapp.com
    Former-commit-id: 32a965a91703cbd517501b6c21e380a99ee18afb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com'
+    domain: 'herokuapp.com'
   }
 
   ActionMailer::Base.delivery_method = :smtp
