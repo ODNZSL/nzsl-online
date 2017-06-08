ff62f769bc6f14bfa4c2f9cdb0168283134cf323
Brenda Wallace <brenda@wallace.net.nz>
2017-06-08 03:02:31 +0000

    Fix up ordering of ordering pages and page parts
    Former-commit-id: fc11a4ee3aa3bdcf4b95928570c5230019ed5ac1

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,9 +1,14 @@
 $(function(){
-
   // $('.ckeditor').ckeditor();
-  $('ul.record').sortable({containment: 'parent', update: function(event, ui) {
-    new_order = [];
-    $('ul.record li').each(function() { new_order.push($(this).attr('id').replace('id', '')); });
+  $('tbody.sortable').sortable({containment: 'parent', update: function(/* event, ui*/) {
+    var new_order = [];
+    console.log(new_order);
+    $('tbody.sortable tr').each(function() {
+      var page_id = $(this).attr('id').replace('id', '');
+      new_order.push(page_id);
+      console.log(new_order);
+    });
+    console.log(new_order);
     var reorder_path = window.location.pathname.replace(/((edit|new)\/?)$/, 'page_parts').replace(/(\/)$/,'') + '/reorder';
     $.post(reorder_path, {'items[]': new_order});
   }});
