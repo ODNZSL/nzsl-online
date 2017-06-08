9e8e5cde33abf8ed880568fadc02bad08cad3864
Brenda Wallace <brenda@wallace.net.nz>
2017-06-08 00:44:36 +0000

    Class added nothing useful to seeds - so gone
    Former-commit-id: 63c6dd2dabf8af54250d1c68ece0ed748bbd551c

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,24 @@
+# frozen_string_literal: true
 require 'csv'
-class CsvSeedLoader
-  def load!
-    load(:setting)
-    load(:page)
-    load(:page_part)
-  end
 
-  private
-
-  def load(data_type)
-    data_klass = data_type.to_s.camelize.constantize
-    data_klass.transaction do
-      CSV.foreach(csv_filename(data_type)) do |row|
-        model = data_klass.create_from_csv!(row)
-        Rails.logger.info "\tCreated #{data_type} #{model}"
-      end
+def load(data_type)
+  data_klass = data_type.to_s.camelize.constantize
+  data_klass.transaction do
+    CSV.foreach(csv_filename(data_type)) do |row|
+      model = data_klass.create_from_csv!(row)
+      Rails.logger.info "\tCreated #{data_type} #{model}"
     end
-    Rails.logger.info("Finished loading #{data_type}")
-    ActiveRecord::Base.connection.reset_pk_sequence!(data_type)
   end
-
-  def csv_filename(data_type)
-    filename = Rails.root.join('db', 'seeds', "#{data_type.to_s.pluralize}.csv")
-    Rails.logger.info "Loading settings from #{filename}..."
-    filename
-  end
+  Rails.logger.info("Finished loading #{data_type}")
+  ActiveRecord::Base.connection.reset_pk_sequence!(data_type)
 end
 
-CsvSeedLoader.new.load!
+def csv_filename(data_type)
+  filename = Rails.root.join('db', 'seeds', "#{data_type.to_s.pluralize}.csv")
+  Rails.logger.info "Loading settings from #{filename}..."
+  filename
+end
+
+load(:setting)
+load(:page)
+load(:page_part)
