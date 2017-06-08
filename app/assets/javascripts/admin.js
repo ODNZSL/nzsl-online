287751477b64eec3dcd1b472b56b24ce8bdc670d
Brenda Wallace <brenda@wallace.net.nz>
2017-06-08 03:03:30 +0000

    Removed debugging
    Former-commit-id: 40dcfdda3090a06f85ea90466936456c38cd397b

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,13 +2,10 @@ $(function(){
   // $('.ckeditor').ckeditor();
   $('tbody.sortable').sortable({containment: 'parent', update: function(/* event, ui*/) {
     var new_order = [];
-    console.log(new_order);
     $('tbody.sortable tr').each(function() {
       var page_id = $(this).attr('id').replace('id', '');
       new_order.push(page_id);
-      console.log(new_order);
     });
-    console.log(new_order);
     var reorder_path = window.location.pathname.replace(/((edit|new)\/?)$/, 'page_parts').replace(/(\/)$/,'') + '/reorder';
     $.post(reorder_path, {'items[]': new_order});
   }});
