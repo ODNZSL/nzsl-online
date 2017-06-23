d63b88ce22c3f32f2e3bf55fcc91076546de3b04
Cara Hill <cara@rabidtech.co.nz>
2017-06-23 15:54:59 +1200

    Refactoring sign model and specs.

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -17,83 +17,85 @@ class Sign
                 :usage_notes, :related_to, :usage, :examples,
                 :inflection_temporal, :inflection_manner_and_degree, :inflection_plural
 
-  # class #
-  def self.first(params)
-    _count, entries = search(params)
-    return nil if entries.empty?
-    SignParser.new(entries.first).build_sign
-  end
-
-  def self.all(params)
-    all_with_count(params)[1]
-  end
-
-  def self.all_with_count(params)
-    signs = []
-    count, entries = search(params)
-    entries.each do |entry|
-      signs << SignParser.new(entry).build_sign
-    end
-    [count, signs]
-  end
-
-  def self.find(all_or_first = :first, params)
-    send(all_or_first, params) if all_or_first == :all || all_or_first == :first
-  end
-
-  def self.random
-    first random: 1
-  end
-
-  def self.paginate(search_query, page_number)
-    start_index = RESULTS_PER_PAGE * (page_number - 1) + 1
-    start_index = 1 if start_index < 1
-    all_with_count search_query.merge(start: start_index, num: RESULTS_PER_PAGE)
-  end
-
-  def self.current_page(per_page, last_result_index, all_result_length)
-    ((last_result_index / all_result_length.to_f) * (all_result_length / per_page.to_f)).round
-  end
-
-  def self.search(params)
-    before_time = Time.now.to_f
-    url = url_for_search(params)
-    xml_document = Nokogiri::XML(open(url))
-    entries = xml_document.css(ELEMENT_NAME)
-    count = xml_document.css('totalhits').inner_text.to_i
-    save_time_elapsed(url, before_time, count)
-
-    [count, entries]
-  end
-
-  def self.save_time_elapsed(url, before_time, count)
-    # how long did that query take?
-    after_time = Time.now.to_f
-    elapsed_time = after_time - before_time
-    Request.create! url: url, elapsed_time: elapsed_time, count: count, query_type: 'Sign.search'
-  end
-
-  def self.url_for_search(query)
-    # The handling of arrays in query strings is different
-    # in the API than in rails
-    return SIGN_URL unless query.is_a?(Hash)
-    query_string = []
-    query.each do |k, v|
-      if v.is_a?(Array)
-        v.each { |ea| query_string << "#{k}=#{CGI.escape(ea.to_s)}" if ea.present? }
-      elsif v.present?
-        query_string << "#{k}=#{CGI.escape(v.to_s)}"
-      end
-    end
-    "#{SIGN_URL}?#{query_string.join('&')}"
-  end
-
-  # instance #
   def borrowed_from
     related_to unless related_to == 'nzsl'
   end
 
   def location
     SignMenu.locations.flatten.find { |l| l.split('.')[2].downcase == location_name }
+  end
+
+  class << self
+    def first(params)
+      _count, entries = search(params)
+      return nil if entries.empty?
+      SignParser.new(entries.first).build_sign
+    end
+
+    def all(params)
+      all_with_count(params)[1]
+    end
+
+    def find(params, all_or_first = :first)
+      send(all_or_first, params) if all_or_first == :all || all_or_first == :first
+    end
+
+    def random
+      first random: 1
+    end
+
+    def paginate(search_query, page_number)
+      start_index = RESULTS_PER_PAGE * (page_number - 1) + 1
+      start_index = 1 if start_index < 1
+      all_with_count search_query.merge(start: start_index, num: RESULTS_PER_PAGE)
+    end
+
+    def current_page(per_page, last_result_index, all_result_length)
+      ((last_result_index / all_result_length.to_f) * (all_result_length / per_page.to_f)).round
+    end
+
+    def url_for_search(query)
+      # The handling of arrays in query strings is different
+      # in the API than in rails
+      return SIGN_URL unless query.is_a?(Hash)
+      query_string = []
+      query.each do |k, v|
+        if v.is_a?(Array)
+          v.each { |ea| query_string << "#{k}=#{CGI.escape(ea.to_s)}" if ea.present? }
+        elsif v.present?
+          query_string << "#{k}=#{CGI.escape(v.to_s)}"
+        end
+      end
+      "#{SIGN_URL}?#{query_string.join('&')}"
+    end
+
+    def search(params)
+      before_time = Time.now.to_f
+      url = url_for_search(params)
+      xml_document = Nokogiri::XML(open(url))
+      entries = xml_document.css(ELEMENT_NAME)
+      count = xml_document.css('totalhits').inner_text.to_i
+      save_time_elapsed(url, before_time, count)
+
+      [count, entries]
+    end
+
+    private
+
+    def all_with_count(params)
+      signs = []
+      count, entries = search(params)
+      entries.each do |entry|
+        signs << SignParser.new(entry).build_sign
+      end
+      [count, signs]
+    end
+
+    def save_time_elapsed(url, before_time, count)
+      # how long did that query take?
+      after_time = Time.now.to_f
+      elapsed_time = after_time - before_time
+      Request.create! url: url, elapsed_time: elapsed_time, count: count, query_type: 'Sign.search'
+    end
   end
 end
--- spec/models/sign_spec.rb
+++ spec/models/sign_spec.rb
@@ -1,74 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe 'Sign', type: :model do
-  it 'must create a new Sign object' do
-    sign = Sign.new
-    expect(sign.is_a?(Sign)).to eq(true)
-  end
-
-  it 'saves a new sign' do
-    sign = Sign.new
-    expect(sign.is_a?(Sign)).to eq(true)
-    # TODO: test passing data into initalizing
-  end
-
-  it 'retrieves two random signs' do
-    sign_one = Sign.random
-    sign_two = Sign.random
-    expect(sign_one.is_a?(Sign)).to eq(true)
-    expect(sign_two.is_a?(Sign)).to eq(true)
-    expect(sign_one.id).not_to eq(sign_two.id)
-  end
-
-  it 'finds a sign' do
-    sign = Sign.first(id: 1000)
-    expect(sign.is_a?(Sign)).to eq(true)
-  end
-
-  it 'must have set class attributes' do
-    expect(SIGN_URL.match(/\Ahttp\:/).is_a?(MatchData)).to eq(true) # rubocop:disable Performance/StartWith
-    expect(ASSET_URL.match(/\Ahttp\:/).is_a?(MatchData)).to eq(true) # rubocop:disable Performance/StartWith
-  end
-
-  it 'must have all attributes of a Sign' do
-    # As per the spec'd requirements of DNZSL:
-    required_attributes = %i[
-      id
-      gloss_main
-      gloss_secondary
-      gloss_minor
-      gloss_maori
-      word_classes
-      inflection
-      age_groups
-      gender_groups
-      video
-      drawing
-      usage_notes
-      contains_numbers
-      is_fingerspelling
-      is_directional
-      is_locatable
-    ]
-    sign = Sign.new
-    required_attributes.each do |att|
-      expect(sign.respond_to?(att)).to eq(true)
-      expect(sign.respond_to?(att.to_s + '=', '')).to eq(true)
+  describe '.new' do
+    it 'must create a new Sign object' do
+      sign = Sign.new
+      expect(sign.is_a?(Sign)).to eq(true)
     end
   end
 
-  it 'must find a single Sign with a simple search string' do
-    search_query = {
-      's' => ['hello'],
-      'hs' => [],
-      'l' => [],
-      'lg' => [],
-      'tag' => [],
-      'usage' => []
-    }
-    page_number = 1
-    search_result = Sign.paginate(search_query, page_number)
-    # we get at least one result
-    expect(search_result.length).to be > 0
+  describe 'attributes' do
+    it 'must have set class attributes' do
+      expect(SIGN_URL.match(/\Ahttp\:/).is_a?(MatchData)).to eq(true) # rubocop:disable Performance/StartWith
+      expect(ASSET_URL.match(/\Ahttp\:/).is_a?(MatchData)).to eq(true) # rubocop:disable Performance/StartWith
+    end
+
+    it 'must have all attributes of a Sign' do
+      # As per the spec'd requirements of DNZSL:
+      required_attributes = %i[
+        id
+        gloss_main
+        gloss_secondary
+        gloss_minor
+        gloss_maori
+        word_classes
+        inflection
+        age_groups
+        gender_groups
+        video
+        drawing
+        usage_notes
+        contains_numbers
+        is_fingerspelling
+        is_directional
+        is_locatable
+      ]
+      sign = Sign.new
+      required_attributes.each do |att|
+        expect(sign.respond_to?(att)).to eq(true)
+        expect(sign.respond_to?(att.to_s + '=', '')).to eq(true)
+      end
+    end
+  end
+
+  describe '.first' do
+    it 'finds a sign' do
+      sign = Sign.first(id: 1000)
+      expect(sign.is_a?(Sign)).to eq(true)
+    end
+  end
+
+  describe '.random' do
+    it 'retrieves two random signs' do
+      sign_one = Sign.random
+      sign_two = Sign.random
+      expect(sign_one.is_a?(Sign)).to eq(true)
+      expect(sign_two.is_a?(Sign)).to eq(true)
+      expect(sign_one.id).not_to eq(sign_two.id)
+    end
+  end
+
+  describe '.paginate' do
+    it 'must find a single Sign with a simple search string' do
+      search_query = {
+        's' => ['hello'],
+        'hs' => [],
+        'l' => [],
+        'lg' => [],
+        'tag' => [],
+        'usage' => []
+      }
+      page_number = 1
+      search_result = Sign.paginate(search_query, page_number)
+      # we get at least one result
+      expect(search_result.length).to be > 0
+    end
   end
 end
