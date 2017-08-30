2e8f844c6da9136bb08cdbe0c486949e02aee18f
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-08-31 11:37:51 +1200

    first attempt to fix foundation equalizer.

--- app/assets/javascripts/foundation-equalizer.js
+++ app/assets/javascripts/foundation-equalizer.js
@@ -1,7 +1,30 @@
+// $(document).ready(function() {
+//   $("#search-container").on("postequalized.zf.equalizer", function() {
+//     debugger;
+//     var resultsArray = [];
+//     $results = $(resultsArray.push(".link_and_drawing_container"));
+//     $results.each(function(){
+//       console.log("found");
+//       $(this).css("position", "absolute");
+//       $(this).css("bottom", "0");
+//     });
+//     $('#search-container').fadeIn("slow");
+//   });
+//   $("#search-container").on("resizeme.zf.trigger mutateme.zf.trigger", function() {
+//     var resultsArray = [];
+//     $results = $(".link_and_drawing_container");
+//     $results.each(function(){
+//       resultsArray.push($(this).css("position", "relative"));
+//     });
+//   });
+// });
+
 $(document).ready(function() {
   $("#search-container").on("postequalized.zf.equalizer", function() {
+    // debugger;
     $results = $(".link_and_drawing_container");
     $results.each(function(){
+      console.log("found");
       $(this).css("position", "absolute");
       $(this).css("bottom", "0");
     });
