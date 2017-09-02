6c74ae3b2cc58d0bfb4855acf2e2c069d328835e
Brenda Wallace <brenda@wallace.net.nz>
2017-09-03 11:29:59 +1200

    Basic feature spec for the admin page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+require 'capybara/rspec'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
@@ -26,6 +27,8 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -49,5 +52,5 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  config.include SpecHelpers::AuthHelper, type: :controller
+  # config.include SpecHelpers::AuthHelper, type: :controller
 end
--- spec/support/auth_helper.rb
+++
@@ -1,8 +0,0 @@
-module SpecHelpers
-  module AuthHelper
-    def basic_auth(user, password)
-      credentials = ActionController::HttpAuthentication::Basic.encode_credentials user, password
-      request.env['HTTP_AUTHORIZATION'] = credentials
-    end
-  end
-end
