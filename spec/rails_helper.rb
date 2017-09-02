56fdbfb830f5d395626ebdaa6a7cbc963707125f
Brenda Wallace <brenda@wallace.net.nz>
2017-09-03 11:31:34 +1200

    Removal of auth helper - not used anymore

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,6 +51,4 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
-  # config.include SpecHelpers::AuthHelper, type: :controller
 end
