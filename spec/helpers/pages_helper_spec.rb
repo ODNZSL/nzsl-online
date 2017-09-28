e66590c891b2fdb6fd7053381f9b3a10aa340144
Caleb Keene <caleb@rabid.co.nz>
2017-09-29 10:11:05 +1300

    fix rubocop complaints

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe PagesHelper, type: :helper do
   let(:page) do
@@ -9,24 +9,23 @@ RSpec.describe PagesHelper, type: :helper do
     )
   end
 
-  describe "#all_sources_present?" do
-
+  describe '#all_sources_present?' do
     subject { helper.all_sources_present?(page) }
 
-    context "all sources are present" do
-      it "returns true" do
+    context 'all sources are present' do
+      it 'returns true' do
         expect(subject).to be true
       end
     end
 
-    context "source missing" do
+    context 'source missing' do
       before do
         allow_any_instance_of(PagePart)
           .to receive(:translation_path)
-          .and_return("")
+          .and_return('')
       end
 
-      it "returns false" do
+      it 'returns false' do
         expect(subject).to be false
       end
     end
