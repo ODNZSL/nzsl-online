3e9b5a9316d897a1a61f83dc7bbfebd720ed91d5
KatieWright26 <katie.wrght@gmail.com>
2017-10-17 09:53:51 +1300

    change js back to dealing only with vocab bar

--- app/assets/javascripts/vocab_bar.js
+++ app/assets/javascripts/vocab_bar.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
   function hideVocabSheetOnMobile() {
-    var bar = $('.vocab_sheet');
+    var bar = $('.vocab_sheet_bar');
     var windowWidth = $(window).width();
     if(windowWidth < 640 || (bar.length && bar.find('.vocab_sheet_bar_item').length === 0)) {
       bar.hide();
       $('.not_sticky_footer').removeClass('vocab_sheet_background');
-    } else if (windowWidth > 639 && $('.vocab_sheet').length) {
+    } else if (windowWidth > 639 &&  bar.length) {
       bar.show();
       $('.not_sticky_footer').addClass('vocab_sheet_background');
     }
@@ -16,7 +16,7 @@ $(document).ready(function() {
     if($('.search-result-banner').length > 0 && $('.flash').length > 0) {
       $('.vocab_sheet').css("top", "380px")
     } else if ($('.search-result-banner').length > 0 && $('.flash').length < 1) {
-      $('.vocab_sheet').css("top", "340px")
+      $('.vocab_sheet_bar').css("top", "340px")
     } else {
       return
     }
