ca93167ac8b524f9b762819ded2bd0dee6c2312a
KatieWright26 <katie.wrght@gmail.com>
2017-10-16 13:51:21 +1300

    tweak vocab bar positioning

--- app/assets/javascripts/vocab_bar.js
+++ app/assets/javascripts/vocab_bar.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 
   function hideVocabSheetOnMobile() {
     var bar = $('.vocab_sheet_bar');
+    bar.show();
     var windowWidth = $(window).width();
     if((windowWidth < 640) || (bar.length && bar.find('.vocab_sheet_bar_item').length === 0)) {
-      bar.hide();
+      // bar.hide();
       $('.not_sticky_footer').removeClass('vocab_sheet_background');
     } else if ((windowWidth > 639) && ($('.vocab_sheet_bar').length)) {
       bar.show();
@@ -14,9 +15,9 @@ $(document).ready(function() {
 
   function changeVocabLocation() {
     if($('.search-result-banner').length > 0 && $('.flash').length > 0) {
-      $('.vocab_sheet_bar').css("top", "350px")
+      $('.vocab_sheet_bar').css("top", "380px")
     } else if ($('.search-result-banner').length > 0 && $('.flash').length < 1) {
-      $('.vocab_sheet_bar').css("top", "320px")
+      $('.vocab_sheet_bar').css("top", "340px")
     } else {
       return
     }
