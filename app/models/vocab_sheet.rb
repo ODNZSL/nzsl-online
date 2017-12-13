37972179abb90b90321aaf16287200a786105323
KatieWright26 <katie.wrght@gmail.com>
2017-12-14 09:49:44 +1300

    add dependent to has_many relationship so items associated w vocab sheet are also removed

--- app/models/vocab_sheet.rb
+++ app/models/vocab_sheet.rb
@@ -1,7 +1,7 @@
 ##
 # A "sheet" of items (signs) saved by a user
 class VocabSheet < ActiveRecord::Base
-  has_many :items
+  has_many :items, dependent: :destroy
 
   def includes_sign?(sign_id)
     items.any? { |i| i.sign_id == sign_id }
