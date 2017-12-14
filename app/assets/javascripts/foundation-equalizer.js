44b13af24d4a0a582509daa0a92caa46ba2a0e4b
Ben Bradshaw <ben@rabidtech.co.nz>
2017-12-14 17:12:24 +1300

    Revert "Better workaround and JS indenting"
    This reverts commit 88d15c80cc694b85247a577ac26cea00feb23aaa.

--- app/assets/javascripts/foundation-equalizer.js
+++ app/assets/javascripts/foundation-equalizer.js
@@ -1,32 +1,20 @@
 $(document).ready(function() {
     if ($("#search-container").children().length) {
-      $('#search-container').fadeIn(1000);
+    $('#search-container').fadeIn(1000);
     } else {
-      $("#search-container").on("postequalized.zf.equalizer", function() {
-        $results = $(".link_and_drawing_container");
-        $results.each(function(){
-          $(this).css("position", "absolute");
-          $(this).css("bottom", "0");
-        });
-        $('#search-container').fadeIn("slow");
+    $("#search-container").on("postequalized.zf.equalizer", function() {
+      $results = $(".link_and_drawing_container");
+      $results.each(function(){
+        $(this).css("position", "absolute");
+        $(this).css("bottom", "0");
       });
-
-      $("#search-container").on("resizeme.zf.trigger mutateme.zf.trigger", function() {
-        $results = $(".link_and_drawing_container");
-        $results.each(function(){
-          $(this).css("position", "relative");
-        });
+      $('#search-container').fadeIn("slow");
+    });
+    $("#search-container").on("resizeme.zf.trigger mutateme.zf.trigger", function() {
+      $results = $(".link_and_drawing_container");
+      $results.each(function(){
+        $(this).css("position", "relative");
       });
-    }
-});
-
-$(window).on("load", function() {
-  /*
-   * Workaround for postequalized.zf.equalizer not showing
-   * results on a 403 error - should be reviewed if
-   * foundation is updated
-   */
-  if ($("#search-container").children().length) {
-    $('#search-container:hidden').fadeIn(1000);
+    });
   }
 });
