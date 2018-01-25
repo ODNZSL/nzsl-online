a340153097496069d7f2833fb349286f3fbd0fe1
Awesome Code <team@xinminlabs.com>
2018-01-25 01:03:53 +0000

    Auto corrected by following Layout/TrailingWhitespace

--- db/migrate/20110501210932_create_feedbacks.rb
+++ db/migrate/20110501210932_create_feedbacks.rb
@@ -1,10 +1,10 @@
 class CreateFeedbacks < ActiveRecord::Migration
   def self.up
     create_table :feedbacks do |t|
-      
+
       t.string :name
       t.text :message
-      
+
       t.string :video_file_name
       t.integer :video_file_size
       t.string :video_content_type
