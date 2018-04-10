df90d90f1ca97eaa7106daf330207dc9dcbef5b4
dan morrison <dan@rabidtech.co.nz>
2018-04-10 12:12:30 +1200

    change search helper spec classes_for_sign_attribute tests to split results returned from method and test for presence of individual classes instead of full string

--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -6,19 +6,22 @@ RSpec.describe SearchHelper, type: :helper do
   describe '#classes_for_sign_attribute' do
     context 'when provided with a true value for main' do
       it 'returns the main_image class as part of the response string' do
-        expect(helper.send(:classes_for_sign_attribute, :location, true)).to eq('image rounded main_image')
+        classes = helper.send(:classes_for_sign_attribute, :location, true)
+        expect(classes.split(' ')).to include('image', 'rounded', 'main_image')
       end
     end
 
     context 'when provided with an :handshape attribute' do
       it 'returns the transition class as part of the response string' do
-        expect(helper.send(:classes_for_sign_attribute, :handshape, false)).to eq('image rounded transition')
+        classes = helper.send(:classes_for_sign_attribute, :handshape, false)
+        expect(classes.split(' ')).to include('image', 'rounded', 'transition')
       end
     end
 
     context 'when provided with a false value for main and a attribute not matching :handshape' do
       it 'returns the default class string' do
-        expect(helper.send(:classes_for_sign_attribute, nil, false)).to eq('image rounded ')
+        classes = helper.send(:classes_for_sign_attribute, nil, false)
+        expect(classes.split(' ')).to include('image', 'rounded')
       end
     end
   end
