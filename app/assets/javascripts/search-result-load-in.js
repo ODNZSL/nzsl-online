f2833e1825a2f9a8cc82c163c06baca1d4b1656f
danieljauch <daniel.p.jauch@gmail.com>
2018-08-08 11:29:55 +1200

    Code Climate happiness.

--- app/assets/javascripts/search-result-load-in.js
+++ app/assets/javascripts/search-result-load-in.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  var searchResult = 0;
+  let searchResult = 0;
 
-  $(".search-results__card").each(function () {
+  $('.search-results__card').each(function() {
     $(this)
       .css({
-        transitionDelay: (searchResult * 0.05) + "s"
+        transitionDelay: (searchResult * 0.05) + 's',
       })
-      .addClass("show-card");
+      .addClass('show-card');
     searchResult++;
   });
 });
