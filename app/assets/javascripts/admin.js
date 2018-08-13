237e76277ec79292bd52cb320c46dd7eaeda0666
Awesome Code <team@xinminlabs.com>
2018-08-13 23:23:42 +0000

    Auto corrected by following Javascript comma-spacing

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -6,7 +6,7 @@ $(function(){
       var page_id = $(this).attr('id').replace('id', '');
       new_order.push(page_id);
     });
-    var reorder_path = window.location.pathname.replace(/((edit|new)\/?)$/, 'page_parts').replace(/(\/)$/,'') + '/reorder';
+    var reorder_path = window.location.pathname.replace(/((edit|new)\/?)$/, 'page_parts').replace(/(\/)$/, '') + '/reorder';
     $.post(reorder_path, {'items[]': new_order});
   }});
 });
