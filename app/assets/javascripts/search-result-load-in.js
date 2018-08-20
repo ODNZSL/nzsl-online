3b8c58ce56a80fd733b484bad15988cd15583667
louise-r-blue <larblue@rocketmail.com>
2018-08-20 18:40:11 +1200

    Add statement to remove placeholder class

--- app/assets/javascripts/search-result-load-in.js
+++ app/assets/javascripts/search-result-load-in.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   var searchResult = 0; // eslint-disable-line no-var
-
   $('.search-results__card').each(function() {
     $(this)
       .css({
         transitionDelay: (searchResult * 0.05) + 's',
       })
+      .removeClass('search-results--placeholder')
       .addClass('show-card');
     searchResult++;
   });
