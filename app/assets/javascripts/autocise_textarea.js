a6a1f86f70a03f1b15240851a78a713aad4743f8
louise-r-blue <larblue@rocketmail.com>
2018-08-21 10:24:42 +1200

    Change to vanillia js to make more readible

--- app/assets/javascripts/autocise_textarea.js
+++ app/assets/javascripts/autocise_textarea.js
@@ -1,14 +1,23 @@
-// Applied globally on all textareas with the "autoExpand" class
-$(document)
-    .on('focus.auto-expand', 'textarea.auto-expand', function(){
-        var savedValue = this.value;
-        this.value = '';
-        this.baseScrollHeight = this.scrollHeight;
-        this.value = savedValue;
-    })
-    .on('input.auto-expand', 'textarea.auto-expand', function(){
-        var minRows = this.getAttribute('data-min-rows')|0, rows;
-        this.rows = minRows;
-        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
-        this.rows = minRows + rows;
-    });
+// Use an event to listen to all input events and then filter out ones that are not on a textarea
+// if the element is a textarea we will call a auto_expand function
+// and pass in the element as an argument using event.target.
+document.addEventListener('input', function (event) {
+  if (event.target.tagName.toLowerCase() !== 'textarea') return;
+  auto_expand(event.target);
+}, false);
+
+var auto_expand = function (field) {
+  // Reset field height
+  field.style.height = 'inherit';
+  // get computed styles for the element and scrollheight to calculate height of the content.
+  var computed = window.getComputedStyle(field);
+  // Calculate the height
+  var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+               + parseInt(computed.getPropertyValue('padding-top'), 10)
+               + field.scrollHeight
+               + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+               + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+  // Reset height of element using style property
+  field.style.height = height + 'px';
+
+};
