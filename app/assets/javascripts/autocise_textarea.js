80402677c712ddbead87092353161cbab52dc92b
louise-r-blue <larblue@rocketmail.com>
2018-08-21 19:00:07 +1200

    Update js function with comments and if statements

--- app/assets/javascripts/autocise_textarea.js
+++ app/assets/javascripts/autocise_textarea.js
@@ -6,7 +6,14 @@ document.addEventListener('input', function (event) {
   auto_expand(event.target);
 }, false);
 
+
 var auto_expand = function (field) {
+  // check the field parameter class so we can give the same height to other textareas with same class.
+  var check_is_main = field.classList.contains('item_name');
+  var check_is_maori = field.classList.contains('item_maori_name');
+  // get all the textareas with class of the main and maori gloss
+  var all_item_name = document.getElementsByClassName('item_name');
+  var all_item_maori_name = document.getElementsByClassName('item_maori_name');
   // Reset field height
   field.style.height = 'inherit';
   // get computed styles for the element and scrollheight to calculate height of the content.
@@ -20,4 +27,17 @@ var auto_expand = function (field) {
   // Reset height of element using style property
   field.style.height = height + 'px';
 
+  // add the height to all textareas with same class name for main glosses and maori glosses
+  if (check_is_main) {
+    for(var i = 0; i < all_item_name.length; i++) {
+      all_item_name[i].style.height = height + 'px';
+      console.log("Title gloss length: " + all_item_name.length);
+    }
+  }
+  if (check_is_maori) {
+    for(var i = 0; i < all_item_maori_name.length; i++) {
+      all_item_maori_name[i].style.height = height + 'px';
+      console.log("Title gloss length: " + all_item_maori_name.length);
+    }
+  }
 };
