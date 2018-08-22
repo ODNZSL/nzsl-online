1de6640c82c3116781c500f6ef30603fd484e759
danieljauch <daniel.p.jauch@gmail.com>
2018-08-22 16:13:37 +1200

    Yay, JavaScript way now works! However, we don't Ajax update the vocab list yet.

--- app/assets/javascripts/search-vocab.js
+++ app/assets/javascripts/search-vocab.js
@@ -1,17 +1,15 @@
-// $(document).ready(function() {
-//   $(".add-to-vocab-btn").click(function (e) {
-//     e.preventDefault();
-//     var sign_id = $(this).attr("data-sign-id");
+$(document).ready(function() {
+  $(".add-to-vocab-btn").click(function (e) {
+    e.preventDefault();
+    var sign_id = $(this).attr("data-sign-id");
 
-//     $.ajax({
-//       url: "/vocab_sheet/items/",
-//       method: "POST",
-//       data: { sign_id },
-//       headers: new Headers(
-
-//       )
-//     }).done(function (data) {
-//       console.log(data);
-//     });
-//   });
-// });
+    $.ajax({
+      url: "/vocab_sheet/items/",
+      method: "POST",
+      data: { sign_id },
+      headers: { 'X-CSRF-Token': $('meta[name="authenticity-token"]').attr('content') }
+    }).done(function (data) {
+      console.log(data);
+    });
+  });
+});
