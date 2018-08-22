9407da1dd32e5836a2de7c2beec7243e569bae83
danieljauch <daniel.p.jauch@gmail.com>
2018-08-23 08:59:48 +1200

    Ajax response now asychronously adds the sign to the vocab lists and displays success / error messages in the vocab partial after response.

--- app/assets/javascripts/search-vocab.js
+++ app/assets/javascripts/search-vocab.js
@@ -3,13 +3,29 @@ $(document).ready(function() {
     e.preventDefault();
     var sign_id = $(this).attr("data-sign-id");
 
+    var successNotice = '<div class="flash vocab_var_notice ajax-success-response">You have added a sign to your vocab sheet.</div>';
+    var failureNotice = '<div class="flash vocab_var_notice ajax-failure-response">There was an error adding a sign to your vocab sheet.</div>';
+    var vocabList = ".vocab_sheet_bar ul";
+
     $.ajax({
       url: "/vocab_sheet/items/",
       method: "POST",
       data: { sign_id },
       headers: { 'X-CSRF-Token': $('meta[name="authenticity-token"]').attr('content') }
     }).done(function (data) {
-      console.log(data);
+      $(successNotice).insertBefore(vocabList);
+      $(vocabList).append(data);
+
+      setTimeout(function () {
+        $(".ajax-success-response").remove();
+      }, 2000);
+    }).fail(function (error) {
+      console.error(error.statusText);
+      $(failureNotice).insertBefore(vocabList);
+
+      setTimeout(function () {
+        $(".ajax-failure-response").remove();
+      }, 2000);
     });
   });
 });
