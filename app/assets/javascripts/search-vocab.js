ca7969f4c7b5b58609348f23bc9b973ae16e7f50
danieljauch <daniel.p.jauch@gmail.com>
2018-08-22 15:58:15 +1200

    Teeeechnically this works the same way that the old one does, but I want to take more time on this to get it done with Ajax since we'll need that later on anyway, and I'd rather build out the new functionality so we can use it moving forward rather than piling on the technical debt by putting my eggs in the old basket. I'm putting this here in case there are time constraints that require me to cop out with this method

---
+++ app/assets/javascripts/search-vocab.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+//   $(".add-to-vocab-btn").click(function (e) {
+//     e.preventDefault();
+//     var sign_id = $(this).attr("data-sign-id");
+
+//     $.ajax({
+//       url: "/vocab_sheet/items/",
+//       method: "POST",
+//       data: { sign_id },
+//       headers: new Headers(
+
+//       )
+//     }).done(function (data) {
+//       console.log(data);
+//     });
+//   });
+// });
