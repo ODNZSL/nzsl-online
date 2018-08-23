1b300a176e5b5ae2fa689383a9270cc0270a1d75
danieljauch <daniel.p.jauch@gmail.com>
2018-08-23 13:07:04 +1200

    Last helpful advice change from dan

--- app/assets/javascripts/search-vocab.js
+++ app/assets/javascripts/search-vocab.js
@@ -1,38 +1,43 @@
 $(document).ready(function() {
-  $('.add-to-vocab-btn').click(function(e) {
-    e.preventDefault();
-    var signId = $(this).attr('data-sign-id');
+  if ($('.add-to-vocab-btn').length > 0) {
     var notice = '.vocab_sheet .ajax-flash';
     var vocabList = '.vocab_sheet_bar ul';
 
-    $.ajax({
-      url: '/vocab_sheet/items/',
-      method: 'POST',
-      data: {
-        sign_id: signId,
-      },
-      headers: {
-        'X-CSRF-Token': $('meta[name="authenticity-token"]').attr('content'),
-      },
-    }).done(function(data) {
-      onVocabItemAdded(data);
-    }).fail(function(error) {
-      onVocabItemError(error.statusText);
+    $('.add-to-vocab-btn').click(function(e) {
+      e.preventDefault();
+      addVocabItem($(this).attr('data-sign-id'));
     });
-
+  
+    function addVocabItem(signId) {
+      $.ajax({
+        url: '/vocab_sheet/items/',
+        method: 'POST',
+        data: {
+          sign_id: signId,
+        },
+        headers: {
+          'X-CSRF-Token': $('meta[name="authenticity-token"]').attr('content'),
+        },
+      }).done(function(data) {
+        onVocabItemAdded(data);
+      }).fail(function(error) {
+        onVocabItemError(error.statusText);
+      });
+    }
+  
     function onVocabItemAdded(htmlElem) {
       if ($('.vocab_sheet_bar').css('display') === 'none') {
         $('.vocab_sheet_bar').show();
       }
-
+  
       $(notice).show().text('You have added a sign to your vocab sheet.');
       $(vocabList).append(htmlElem);
       hideNotice();
     }
-
+  
     function onVocabItemError(errorMessage) {
       console.error(errorMessage);
-
+  
       if ($('.vocab_sheet_bar').css('display') !== 'none') {
         $(notice).show().text(
           'There was an error adding a sign to your vocab sheet.'
@@ -46,11 +51,11 @@ $(document).ready(function() {
         );
       }
     }
-
+  
     function hideNotice() {
       setTimeout(function() {
         $(notice).hide();
       }, 2000);
     }
-  });
+  }
 });
