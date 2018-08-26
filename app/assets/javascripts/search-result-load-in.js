f05d0ffe415b0a3bf98b54064d5214b21ec4888d
louise-r-blue <larblue@rocketmail.com>
2018-08-27 09:34:19 +1200

    Remove trailing 0 and add conditional to check if search results exist

--- app/assets/javascripts/search-result-load-in.js
+++ app/assets/javascripts/search-result-load-in.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   var searchResult = 0; // eslint-disable-line no-var
-  $('.search-results__card').each(function() {
-    $(this)
-      .css({
-        transitionDelay: (searchResult * 0.10) + 's',
-      })
-      .addClass('show-card');
-    searchResult++;
-  });
-  $(window).on('load', function() {
-    $('.search-results__card').removeClass('search-results--placeholder');
-  });
+  if ($('.search-results__card').length > 0) {
+    $('.search-results__card').each(function() {
+      $(this)
+        .css({
+          transitionDelay: (searchResult * 0.1) + 's',
+        })
+        .addClass('show-card');
+      searchResult++;
+    });
+    }
+    $(window).on('load', function() {
+      $('.search-results__card').removeClass('search-results--placeholder');
+    });
 });
