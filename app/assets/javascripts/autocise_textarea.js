52c3222d688a5dcf1c7e149575d3ab48a4cb3681
louise-r-blue <larblue@rocketmail.com>
2018-08-27 10:08:02 +1200

    Add document ready fn to check an adjust textarea size

--- app/assets/javascripts/autocise_textarea.js
+++ app/assets/javascripts/autocise_textarea.js
@@ -1,12 +1,17 @@
+// Use document ready and call the autoExpandTextarea function to display all edited text in textareas
+$(document).ready(function() {
+  autoExpandTextarea(textfield);
+}
+
 // Use an event to listen to all input events and then filter out ones that are not on a textarea
 // if the element is a textarea we will call a auto_expand function
 // and pass in the element as an argument using event.target.
 document.addEventListener('input', function (event) {
   if (event.target.tagName.toLowerCase() !== 'textarea') return;
-  auto_expand(event.target);
+  autoExpandTextarea(event.target);
 }, false);
 
-var auto_expand = function (field) {
+var autoExpandTextarea = function (field) {
   // check the field parameter class so we can give the same height to other textareas with same class.
   var check_is_main = field.classList.contains('item_name');
   var check_is_maori = field.classList.contains('item_maori_name');
@@ -26,10 +31,11 @@ var auto_expand = function (field) {
                + field.scrollHeight
                + parseInt(computed.getPropertyValue('padding-bottom'), 10)
                + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
-  // Reset height of element using style property or just use the ifs
+  // Reset height of element using style property or just use the ifs?
   field.style.height = height + 'px';
 
   // add the height to all textareas with same class name for main glosses and maori glosses
+  // as we want to keep each row at the same level
   if (check_is_main) {
     for(var i = 0; i < all_item_name.length; i++) {
       all_item_name[i].style.height = height + 'px';
