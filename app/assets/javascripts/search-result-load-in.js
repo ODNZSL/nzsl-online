79a56ff8209ff0a4adbc99dc412ad3c3e07be047
louise-r-blue <larblue@rocketmail.com>
2018-08-27 11:07:53 +1200

    Add conditional to window on load inside doc ready fn

--- app/assets/javascripts/search-result-load-in.js
+++ app/assets/javascripts/search-result-load-in.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   var searchResult = 0; // eslint-disable-line no-var
-  if ($('.search-results__card').length > 0) {
-    $('.search-results__card').each(function() {
-      $(this)
-        .css({
-          transitionDelay: (searchResult * 0.1) + 's',
-        })
-        .addClass('show-card');
+  $('.search-results__card').each(function() {
+    $(this)
+      .css({
+        transitionDelay: (searchResult * 0.1) + 's',
+      })
+      .addClass('show-card');
       searchResult++;
-    });
+  });
+    if ($('.search-results__card').length > 0) {
+      $(window).on('load', function() {
+        $('.search-results__card').removeClass('search-results--placeholder');
+      });
     }
-    $(window).on('load', function() {
-      $('.search-results__card').removeClass('search-results--placeholder');
-    });
 });
