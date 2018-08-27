f6bc05457561b8a7abbd3f5f9784f2fb7b718fec
louise-r-blue <larblue@rocketmail.com>
2018-08-28 10:09:28 +1200

    Change comment line length and use camel case - its JSgit add app/assets/javascripts/autocise_textarea.jsgit add app/assets/javascripts/autocise_textarea.js

--- app/assets/javascripts/autocise_textarea.js
+++ app/assets/javascripts/autocise_textarea.js
@@ -1,5 +1,5 @@
-// Need to set max height when user loads, reloads or refreshes their vocab sheet
-// Get the max height and apply to all textareas when page loads.
+// Get the max height and apply
+// to all textareas when page loads/reloads
 
 $(document).ready(function() {
   var numberArray = [];
@@ -11,27 +11,31 @@ $(document).ready(function() {
   });
 });
 
-// Use an event to listen to all input events and then filter out ones that are not on a textarea
-// if the element is a textarea we will call a autoExpandTextarea function
-// and pass in the element as an argument using event.target.
-document.addEventListener('input', function (event) {
+// Use an event to listen to all input events and then
+// filter out ones that are not on a textarea
+// if the element is a textarea we will call a
+// autoExpandTextarea function and pass in
+// the element as an argument using event.target.
+document.addEventListener('input', function(event) {
   if (event.target.tagName.toLowerCase() !== 'textarea') return;
   autoExpandTextarea(event.target);
 }, false);
 
-var autoExpandTextarea = function (field) {
-  // check the field parameter class so we can give the same height to other textareas with same class.
-  var check_is_main = field.classList.contains('item_name');
-  var check_is_maori = field.classList.contains('item_maori_name');
+var autoExpandTextarea = function(field) {
+  // check the field parameter class so we can give the
+  // same height to other textareas with same class.
+  var checkIsMain = field.classList.contains('item_name');
+  var checkIsMaori = field.classList.contains('item_maori_name');
 
   // get all the textareas with class of the main and maori gloss
-  var all_item_name = document.getElementsByClassName('item_name');
-  var all_item_maori_name = document.getElementsByClassName('item_maori_name');
+  var allItemName = document.getElementsByClassName('item_name');
+  var allItemMaoriName = document.getElementsByClassName('item_maori_name');
 
   // Reset field height
   field.style.height = 'inherit';
 
-  // get computed styles for the element and scrollheight to calculate height of the content.
+  // get computed styles for the element and
+  // scrollheight to calculate height of the content.
   var computed = window.getComputedStyle(field);
   // Calculate the height
   var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
@@ -44,15 +48,15 @@ var autoExpandTextarea = function (field) {
 
   // add the height to all textareas with same class name for main glosses and maori glosses
   // as we want to keep each row at the same level
-  if (check_is_main) {
-    for(var i = 0; i < all_item_name.length; i++) {
-      all_item_name[i].style.height = height + 'px';
+  if (checkIsMain) {
+    for(var i = 0; i < allItemName.length; i++) {
+      allItemName[i].style.height = height + 'px';
     }
   }
 
-  if (check_is_maori) {
-    for(var i = 0; i < all_item_maori_name.length; i++) {
-      all_item_maori_name[i].style.height = height + 'px';
+  if (checkIsMaori) {
+    for(var i = 0; i < allItemMaoriName.length; i++) {
+      allItemMaoriName[i].style.height = height + 'px';
     }
   }
 };
