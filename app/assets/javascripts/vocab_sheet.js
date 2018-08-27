8711cedf629f7eab3ff5fe5e17bbed93dfeb6fa6
danieljauch <daniel.p.jauch@gmail.com>
2018-08-28 09:55:08 +1200

    Added a typing timeout because the every keypress submit was causing delay issues

--- app/assets/javascripts/vocab_sheet.js
+++ app/assets/javascripts/vocab_sheet.js
@@ -64,8 +64,7 @@ $(document).ready(function() {
     var formAction = textBox.closest('form').attr('action');
     var signId = formAction.split('/')[-1];
     var notes = '';
-    var updateInProgress = false;
-    var xhr = null;
+    var typeTimer = null;
 
     var checkCharacterCount = function() {
       textBox.each(function() {
@@ -104,22 +103,21 @@ $(document).ready(function() {
     }
 
     function setCharacterCount(elem) {
-      notes = elem.val();
+      clearTimeout(typeTimer);
 
-      elem.siblings('.character-count__wrap')
-        .children('.character-count__count')
-        .text(500 - notes.length);
+      typeTimer = setTimeout(function() {
+        notes = elem.val();
+  
+        elem.siblings('.character-count__wrap')
+          .children('.character-count__count')
+          .text(500 - notes.length);
 
-      if (updateInProgress) {
-        xhr.abort();
-        updateInProgress = false;
-      }
-
-      updateNotes(formAction, signId, notes);
+        updateNotes(formAction, signId, notes);
+      }, 100);
     }
 
     function updateNotes(action, signId, notes) {
-      xhr = $.ajax({
+      $.ajax({
         url: action,
         method: 'PUT',
         data: {
