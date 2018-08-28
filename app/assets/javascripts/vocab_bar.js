f9680ce03714f19ae6da2066d1dbc62d89022db7
danieljauch <daniel.p.jauch@gmail.com>
2018-08-28 12:02:34 +1200

    BEM-ed all the classnames, restyled according to the design, made the alerts with the new style and added a transition, stickied the container with JS, added the fade at the bottom of the scroll, and made sure it doesn't flow over the footer.

--- app/assets/javascripts/vocab_bar.js
+++ app/assets/javascripts/vocab_bar.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
-  function hideVocabSheetOnMobile() {
-    var bar = $('.vocab_sheet_bar');
-    var windowWidth = $(window).width();
-    if (windowWidth < 640 || (bar.length && bar.find('.vocab_sheet_bar_item').length === 0)) {
-      bar.hide();
-      $('.not_sticky_footer').removeClass('vocab_sheet_background');
-    } else if (windowWidth > 639 &&  bar.length) {
-      bar.show();
-      $('.not_sticky_footer').addClass('vocab_sheet_background');
+  if ($('.vocab-sheet__bar').length > 0) {
+    var bar = $('.vocab-sheet__bar');
+
+    function hideVocabSheetOnMobile() {
+      var windowWidth = $(window).width();
+      if (windowWidth < 640 || (bar.length && bar.find('.vocab-sheet__bar-item').length === 0)) {
+        bar.hide();
+        $('.not_sticky_footer').removeClass('vocab_sheet_background');
+      } else if (windowWidth > 639 &&  bar.length) {
+        bar.show();
+        $('.not_sticky_footer').addClass('vocab_sheet_background');
+      }
     }
-  }
 
-  $(window).resize(function() {
+    function positionVocabBar() {
+      var searchResultBanner = $('.search-result-banner');
+      var offset = searchResultBanner.offset().top;
+      var bottom = offset + searchResultBanner.outerHeight(true);
+      var baseMargin = 24; // 1.5rem
+      var initialOffset = bottom + baseMargin;
+      var windowScroll = $(window).scrollTop();
+      var footerTop = $('.sticky_footer').offset().top;
+      var barBottom = bar.offset().top + bar.outerHeight(true) + baseMargin;
+
+      if (windowScroll < bottom) {
+        bar.css({ top: initialOffset - offset });
+      } else if (barBottom < footerTop || 
+        (windowScroll < footerTop - (bar.outerHeight(true) + baseMargin))) {
+        bar.css({ top: windowScroll - offset + baseMargin });
+      }
+    }
+
+    $(window).resize(function() {
+      hideVocabSheetOnMobile();
+    });
+
+    $(window).scroll(function() {
+      positionVocabBar();
+    });
+
+    positionVocabBar();
     hideVocabSheetOnMobile();
-  });
-
-  hideVocabSheetOnMobile();
+  }
 });
