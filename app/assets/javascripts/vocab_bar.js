12c54797d5730e48e51e3ba997f36cf2c89a2104
danieljauch <daniel.p.jauch@gmail.com>
2018-08-28 15:37:36 +1200

    Took out excess function / class

--- app/assets/javascripts/vocab_bar.js
+++ app/assets/javascripts/vocab_bar.js
@@ -1,26 +1,15 @@
 $(document).ready(function() {
   if ($('.vocab-sheet__bar').length > 0) {
     var bar = $('.vocab-sheet__bar');
-
-    function hideVocabSheetOnMobile() {
-      var windowWidth = $(window).width();
-      if (windowWidth < 640 || (bar.length && bar.find('.vocab-sheet__bar-item').length === 0)) {
-        bar.hide();
-        $('.not_sticky_footer').removeClass('vocab_sheet_background');
-      } else if (windowWidth > 639 &&  bar.length) {
-        bar.show();
-        $('.not_sticky_footer').addClass('vocab_sheet_background');
-      }
-    }
+    var searchResultBanner = $('.search-result-banner');
+    var offset = searchResultBanner.offset().top;
+    var bottom = offset + searchResultBanner.outerHeight(true);
+    var baseMargin = 24; // 1.5rem
+    var initialOffset = bottom + baseMargin;
+    var footerTop = $('.sticky_footer').offset().top;
 
     function positionVocabBar() {
-      var searchResultBanner = $('.search-result-banner');
-      var offset = searchResultBanner.offset().top;
-      var bottom = offset + searchResultBanner.outerHeight(true);
-      var baseMargin = 24; // 1.5rem
-      var initialOffset = bottom + baseMargin;
       var windowScroll = $(window).scrollTop();
-      var footerTop = $('.sticky_footer').offset().top;
       var barBottom = bar.offset().top + bar.outerHeight(true) + baseMargin;
 
       if (windowScroll < bottom) {
@@ -31,15 +20,10 @@ $(document).ready(function() {
       }
     }
 
-    $(window).resize(function() {
-      hideVocabSheetOnMobile();
-    });
-
     $(window).scroll(function() {
       positionVocabBar();
     });
 
     positionVocabBar();
-    hideVocabSheetOnMobile();
   }
 });
