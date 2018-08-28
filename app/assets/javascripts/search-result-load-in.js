5f8414820d84542ddd07def574c8763b49b9ffad
louise-r-blue <larblue@rocketmail.com>
2018-08-29 11:18:18 +1200

    Add a transition delay to stagger the removal of the placeholder by changing opacity

--- app/assets/javascripts/search-result-load-in.js
+++ app/assets/javascripts/search-result-load-in.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-  var searchResult = 0; // eslint-disable-line no-var
-  $('.search-results__card').each(function() {
-    $(this)
-      .css({
-        transitionDelay: (searchResult * 0.1) + 's',
-      })
-      .addClass('show-card');
-      searchResult++;
-  });
-    if ($('.search-results__card').length > 0) {
-      $(window).on('load', function() {
-        $('.search-results__card').removeClass('search-results--placeholder');
+  if ($('.search-results__card').length > 0) {
+    var searchResult = 0; // eslint-disable-line no-var
+    var searchResultPlaceholder = 0;
+    $('.search-results__card').each(function() {
+      $(this)
+        .css({
+          transitionDelay: (searchResult * 0.1) + 's',
+        })
+        .addClass('show-card');
+        searchResult++;
+    });
+    $(window).on('load', function() {
+      $('.search-results--placeholder').each(function() {
+        $(this)
+          .css({
+            transitionDelay: (searchResultPlaceholder * 0.1) + 's',
+          })
+          .addClass('hide-placeholder');
+          searchResultPlaceholder++;
       });
-    }
+    });
+  };
 });
