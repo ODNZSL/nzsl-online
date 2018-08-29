5afecd2f650add32f1dcaafdf3899f136e4e6be3
louise-r-blue <larblue@rocketmail.com>
2018-08-30 09:39:55 +1200

    Rename fn to be shorter

--- app/assets/javascripts/search-result-load-in.js
+++ app/assets/javascripts/search-result-load-in.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
     var searchResult = 0; // eslint-disable-line no-var
     var searchResultPlaceholder = 0;
     $('.search-results__card').each(function() {
-      searchResultTransition($(this), searchResult, 'show-card');
+      resultTransition($(this), searchResult, 'show-card');
       searchResult++;
     });
 
     $(window).on('load', function() {
       $('.search-results--placeholder').each(function() {
-        searchResultTransition($(this), searchResultPlaceholder, 'hide-placeholder');
+        resultTransition($(this), searchResultPlaceholder, 'hide-placeholder');
         searchResultPlaceholder++;
       });
     });
 
-    function searchResultTransition(searchResultItem, counter, transitionClass) {
+    function resultTransition(searchResultItem, counter, transitionClass) {
       searchResultItem
         .css({
           transitionDelay: (counter * 0.1) + 's',
