ed837588928beeb00da3cff5a9b6fc705d49525b
louise-r-blue <larblue@rocketmail.com>
2018-09-12 14:02:43 +1200

    Alter padding to make container more aligned and remove hide back button fn

--- app/assets/javascripts/search_result_link.js
+++ app/assets/javascripts/search_result_link.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
     window.location = $(this).find('a').attr('href');
   });
 
-  function hideBackButton() {
-    if ($('.back_to_search_results').length === 0) {
-      $('.back-arrow-icon').css('display', 'none');
-    } else {
-      $('.back-arrow-icon').css('display', 'block');
-    }
-  }
-
-  hideBackButton();
+  // function hideBackButton() {
+  //   if ($('.back_to_search_results').length === 0) {
+  //     $('.back-arrow-icon').css('display', 'block');
+  //   } else {
+  //     $('.back-arrow-icon').css('display', 'block');
+  //   }
+  // }
+  // 
+  // hideBackButton();
 });
