d202b072ee4c3f5aa5725040be4321e3a0212a14
dan morrison <dan@rabidtech.co.nz>
2018-09-18 09:17:29 +1200

    add rendered pdf model to be used in customising pdf variables (currently name and type) when generating pdfs

---
+++ app/models/rendered_pdf.rb
@@ -0,0 +1,15 @@
+class RenderedPdf
+  attr_reader :file_path
+
+  def initialize(file_path:)
+    @file_path = file_path
+  end
+
+  def download_as_filename
+    "vocab_sheet.pdf"
+  end
+
+  def mime_type
+    "application/pdf"
+  end
+end
