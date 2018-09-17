c27d2422e64c12ed49d4c0647aa362e72c7b7fd5
Awesome Code <team@xinminlabs.com>
2018-09-17 03:10:41 +0000

    Auto corrected by following Ruby Space

--- spec/controllers/signs_controller_spec.rb
+++ spec/controllers/signs_controller_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 RSpec.describe SignsController, type: :controller do
   describe '#show' do
-    before { get :show, params:{ id: '1301' } }
+    before { get :show, params: { id: '1301' } }
 
     it { expect(response).to have_http_status(:ok) }
   end
 
   describe '#search' do
     describe 'simple query' do
-      before { get :search, params:{ s: 'hello' } }
+      before { get :search, params: { s: 'hello' } }
 
       it { expect(response).to have_http_status(:ok) }
     end
