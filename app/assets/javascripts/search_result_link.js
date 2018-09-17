c6dd44bf5a58095362e4b8e34d85e6b823d08be6
louise-r-blue <larblue@rocketmail.com>
2018-09-17 12:00:26 +1200

    Try adding history api call in back button

--- app/assets/javascripts/search_result_link.js
+++ app/assets/javascripts/search_result_link.js
@@ -3,13 +3,16 @@ $(document).ready(function() {
     window.location = $(this).find('a').attr('href');
   });
 
-  // function hideBackButton() {
-  //   if ($('.back_to_search_results').length === 0) {
-  //     $('.back-arrow-icon').css('display', 'block');
-  //   } else {
-  //     $('.back-arrow-icon').css('display', 'block');
-  //   }
-  // }
+  function hideBackButton() {
+    if ($('.back_to_search_results').length === 0 ) {
+      $('.back-arrow-icon').css('display', 'block');
+      // var backTo = '<a href=“javascript:history.go(-2)”>Back to search results</a>'
+      var backTo = '<a href="">Back to search results</a>'
+      $('.back-button-container').append(backTo);
+    } else {
+      $('.back-arrow-icon').css('display', 'block');
+    }
+  }
 
-  // hideBackButton();
+  hideBackButton();
 });
