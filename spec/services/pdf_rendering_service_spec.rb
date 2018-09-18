22d13e1fc0a643a9eb1bd3330dac2e4cf3bba053
dan morrison <dan@rabidtech.co.nz>
2018-09-18 12:10:02 +1200

    add services spec folder and include tests for pdf rendering service

---
+++ spec/services/pdf_rendering_service_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe PdfRenderingService do
+  let(:html) do
+    <<~EO_HTML
+      <html>
+        <head>
+          <title>I am title</title>
+        </head>
+        <body>
+          <h1>This is a test document</h1>
+        </body>
+      </html>
+    EO_HTML
+  end
+
+  subject { PdfRenderingService.new(from_html: html) }
+
+  describe "#render" do
+    let(:pdf_file_header_bytes) { "%PDF-1.4" } # PDFs begin with this sequence of bytes
+
+    it "adds a base tag immediately after the <head> opening tag in the given HTML" do
+      subject.render
+      expect(subject.html).to match(/<head><base href=/)
+    end
+
+    it "Saves a PDF version of the HTML file to disk" do
+      subject.render
+      expect(subject.html).to match(/<head><base href=/)
+      pdf_file_path = subject.pdf.file_path
+      first_bytes_in_file = File.read(pdf_file_path, 8)
+
+      expect(File.exist?(pdf_file_path)).to eq(true)
+      expect(File.size(pdf_file_path)).to be > 0
+      expect(first_bytes_in_file).to eq(pdf_file_header_bytes)
+    end
+  end
+end
