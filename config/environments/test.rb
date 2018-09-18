62d6bf1839939a058c70d870aeec9c75e2331c50
Awesome Code <team@xinminlabs.com>
2018-09-18 13:13:52 +0000

    Auto corrected by following Ruby Layout/TrailingWhitespace

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
-  
+
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
