6d7999ef9b053c60e1b98c915cb120f7be7c2ccc
dan morrison <dan@rabidtech.co.nz>
2018-09-24 11:37:30 +1200

    configure puppeteer to launch in no sandbox mode for running on heroku

--- bin/render-pdf.js
+++ bin/render-pdf.js
@@ -13,15 +13,14 @@ const puppeteer = require("puppeteer");
 const inputPath = `file://${process.argv[2]}`;
 const outputPath = process.argv[3];
 
-/* Travis needs an argument that is generally not prefered
-for live environments so we se it set it optionally here */
-const puppeteerArgs = process.env["CI"] ? ["--no-sandbox"] : [];
-
 console.log("Input path:", inputPath);
 console.log("Output path:", outputPath);
 
+/* Usually we'd prefer not to launch puppeteer with the no-sandbox argument,
+but due to issues getting it running on Heroku this option is required. */
+
 (async () => {
-  const browser = await puppeteer.launch({args: puppeteerArgs});
+  const browser = await puppeteer.launch({args: ["--no-sandbox"]});
   const page = await browser.newPage();
 
   console.log("Starting PDF conversion");
