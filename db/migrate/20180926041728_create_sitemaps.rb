cec672ace99f1ad1af940942f6626139d39bde09
dan morrison <dan@rabidtech.co.nz>
2018-10-01 11:33:19 +1300

    change string type column for xmldump to text type

--- db/migrate/20180926041728_create_sitemaps.rb
+++ db/migrate/20180926041728_create_sitemaps.rb
@@ -1,7 +1,7 @@
 class CreateSitemaps < ActiveRecord::Migration[5.2]
   def change
     create_table :sitemaps do |t|
-      t.string :xmldump
+      t.text :xmldump
       
       t.timestamps
     end
