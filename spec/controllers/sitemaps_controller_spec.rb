c8b0b588d648ccece7a5ac45e625963706fce710
dan morrison <dan@rabidtech.co.nz>
2018-10-02 12:13:39 +1300

    add sitemaps controller file with test outlines

---
+++ spec/controllers/sitemaps_controller_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SitemapsController, type: :controller do
+  
+  describe "#index" do
+    it "returns 200" do
+    end
+
+    it "renders xml" do
+    end
+  end
+
+  describe "#sitemap_builder" do
+    it "returns a new instance of the SitemapBuilder object" do
+
+    end
+  end
+
+end
