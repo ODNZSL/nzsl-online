eba0739a62bb85581cf764054beec4e57f254ff9
dan morrison <dan@rabidtech.co.nz>
2018-10-02 10:39:55 +1300

    breakout slug and sign url setting into individual methods. Allow generate xml method to accept an array of urls to generate xml from

--- app/models/sitemap_builder.rb
+++ app/models/sitemap_builder.rb
@@ -1,22 +1,19 @@
 class SitemapBuilder
 
   def first_or_generate 
-    Sitemap.first || Sitemap.create(xml: generate_xml)
+    Sitemap.first || Sitemap.create(xml: generate_xml(page_slugs + sign_urls))
   end
 
   def update_sitemap
-    Sitemap.first.update(xml: generate_xml)
+    Sitemap.first.update(xml: generate_xml(page_slugs + sign_urls))
   end
 
-  def generate_xml
-    page_slugs = Page.pluck(:slug)
-    sign_ids = fetch_data_dump.map(&:id)
-    base_url = "#{ENV['APP_PROTOCOL']}://#{ENV['APP_DOMAIN_NAME']}/"
+  def generate_xml(items)
+    base_url = Rails.application.config.base_url
 
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") do
-        page_slugs.each { |slug| xml.url { xml.loc base_url + slug } }
-        sign_ids.each { |id| xml.url { xml.loc "#{base_url}signs/#{id}" } }
+        items.each { |item| xml.url { xml.loc base_url + item } }
       end
     end
     builder.to_xml
@@ -26,6 +23,14 @@ class SitemapBuilder
   
   def fetch_data_dump
     Sign.all(xmldump: 1)
+  end
+
+  def page_slugs
+    Page.pluck(:slug)
+  end
+
+  def sign_urls
+    fetch_data_dump.map { |sign| "signs/#{sign.id}" }
   end
 
 end
