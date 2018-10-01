65943adbbb1b99abb82f5257456678834ac9bfc0
dan morrison <dan@rabidtech.co.nz>
2018-10-02 10:55:46 +1300

    change urls to slugs in sitemapBuilder

--- app/models/sitemap_builder.rb
+++ app/models/sitemap_builder.rb
@@ -1,22 +1,22 @@
 class SitemapBuilder
 
   def first_or_generate 
-    Sitemap.first || Sitemap.create(xml: generate_xml(page_slugs + sign_urls))
+    Sitemap.first || Sitemap.create(xml: generate_xml(page_slugs + sign_slugs))
   end
 
   def update_sitemap
-    Sitemap.first.update(xml: generate_xml(page_slugs + sign_urls))
+    Sitemap.first.update(xml: generate_xml(page_slugs + sign_slugs))
   end
 
-  def generate_xml(urls)
+  def generate_xml(slugs)
     base_url = Rails.application.config.base_url
 
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") do
-        urls.each { |url| xml.url { xml.loc base_url + url } }
+        slugs.each { |slug| xml.url { xml.loc base_url + slug } }
       end
     end
-    
+
     builder.to_xml
   end
 
@@ -30,7 +30,7 @@ class SitemapBuilder
     Page.pluck(:slug)
   end
 
-  def sign_urls
+  def sign_slugs
     fetch_data_dump.map { |sign| "signs/#{sign.id}" }
   end
 
