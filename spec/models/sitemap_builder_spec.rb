a756bcb584e2a2f8144fc785e1380febc12cc9be
dan morrison <dan@rabidtech.co.nz>
2018-10-03 10:56:18 +1300

    add sitemap factory. Add first logic steps to sitmap builder spec

--- spec/models/sitemap_builder_spec.rb
+++ spec/models/sitemap_builder_spec.rb
@@ -3,27 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe 'SitemapBuilder', type: :model do
+  
+  let(:builder) do
+    instance_double(SitemapBuilder, fetch_data_dump: "[{sign:{id:1}, {sign:{id:2}}]")
+  end
 
   describe "#first_or_generate" do
+    let!(:sitemap) { FactoryBot.create(:sitemap) }
+
     context "when a Sitemap record exists" do
       it "returns that record" do
+        expect(builder.first_or_generate).to eq(sitemap)
       end
     end
 
     context "when a Sitemap record does not exist" do
+      before do
+        Sitemap.first.delete
+      end
+
       it "generates one" do
+        expect(builder.first_or_generate).not_to eq(sitemap)
+        expect(Sitemap.first).not_to be_nil
       end
     end
   end
 
   describe "#update_sitemap" do
+    let!(:sitemap) { FactoryBot.create(:sitemap) }
+    
     it "updates the first existing Sitemap record in the database" do
+      expect(builder.update_sitemap.id).to_eq eq(sitemap.id)
     end
   end
 
   describe "#generate_xml" do
     context "when an array of slugs are provided" do
+      let(:slugs) {["contact", "signs/22", "dogs"]}
+      let(:base_url) {Rails.application.config.base_url}
       it "returns the expected set of xml data featuring those slugs" do
+        expect(builder.generate_xml(slugs)).to include("#{base_url}signs/22")
       end
     end
   end
