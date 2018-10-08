1df13e4f902b402e60cc8149a5018d7c1172cd0f
louise-r-blue <larblue@rocketmail.com>
2018-10-09 09:17:56 +1300

    Update sign download test

--- spec/helpers/sign_download_helper_spec.rb
+++ spec/helpers/sign_download_helper_spec.rb
@@ -5,8 +5,9 @@ require 'rails_helper'
 RSpec.describe SignDownloadHelper, type: :helper do
   describe '#download-link' do
     context 'when provided with a "high_resolution" value to download' do
-      it 'returns the sign drawing with "high_resolution" as part of the file name' do
-
+      it 'returns the sign drawing with the text "high_resolution" as part of the file name' do
+        # classes = helper.send(:download-link, :location, true)
+        # expect(classes.split(' ')).to include('image', 'rounded', 'main_image')
       end
     end
   end
