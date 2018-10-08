14581f437fda2c6106cd007ec77b3fa688812408
louise-r-blue <larblue@rocketmail.com>
2018-10-08 19:10:19 +1300

    Add basic rspec test for drawing downloads

---
+++ spec/helpers/sign_download_helper_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SignDownloadHelper, type: :helper do
+  describe '#download-link' do
+    context 'when provided with a "high_resolution" value to download' do
+      it 'returns the sign drawing with "high_resolution" as part of the file name' do
+
+      end
+    end
+  end
+end
