bad99bd1f3e6dd44e6c22580daf781dbc399d338
louise-r-blue <larblue@rocketmail.com>
2018-10-12 15:18:11 +1300

    Add basic rspec to test helper method

--- spec/helpers/sign_download_helper_spec.rb
+++ spec/helpers/sign_download_helper_spec.rb
@@ -2,12 +2,12 @@
 
 require 'rails_helper'
 
-RSpec.describe SignDownloadHelper, type: :helper do
-  describe '#download-link' do
-    context 'when provided with a "high_resolution" value to download' do
+RSpec.describe SignsHelper, type: :helper do
+  describe '#convert_to_high_resolution' do
+    context 'when provided with a "default" sign drawing' do
       it 'returns the sign drawing with the text "high_resolution" as part of the file name' do
-        # classes = helper.send(:download-link, :location, true)
-        # expect(classes.split(' ')).to include('image', 'rounded', 'main_image')
+        drawing = "1234/a_test-default"
+        expect(helper.convert_to_high_resolution(drawing)).to include("high_resolution")
       end
     end
   end
