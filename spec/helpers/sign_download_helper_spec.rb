68478f116b9e9de95a63c643fbda91903afad803
louise-r-blue <larblue@rocketmail.com>
2018-10-18 12:34:29 +1300

    Move vaiable out of it statement

--- spec/helpers/sign_download_helper_spec.rb
+++ spec/helpers/sign_download_helper_spec.rb
@@ -3,11 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe SignsHelper, type: :helper do
+  drawing_1 = "1234/a_test-default"
+
   describe '#convert_to_high_resolution' do
     context 'when provided with a "default" sign drawing' do
       it 'returns the sign drawing with the text "high_resolution" as part of the file name' do
-        drawing = "1234/a_test-default"
-        expect(helper.convert_to_high_resolution(drawing)).to include("high_resolution")
+        expect(helper.convert_to_high_resolution(drawing_1)).to include("high_resolution")
       end
     end
   end
