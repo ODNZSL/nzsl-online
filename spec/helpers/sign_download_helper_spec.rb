c56a3f468934d201359acec6e6bcaaefb46f40e9
louise-r-blue <larblue@rocketmail.com>
2018-11-27 21:20:57 +1300

    Modify rspec tests to reflect updated covert drawing to high res method

--- spec/helpers/sign_download_helper_spec.rb
+++ spec/helpers/sign_download_helper_spec.rb
@@ -3,12 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe SignsHelper, type: :helper do
-  drawing_1 = "1234/a_test-default"
+  drawing_1 = "1234/test01-1234-default.png"
+  drawing_2 = "1234/test02-1234.png"
+  drawing_3 = "1234/test03-default-1234-default.png"
 
   describe '#convert_to_high_resolution' do
-    context 'when provided with a "default" sign drawing' do
+    context 'when provided with a "default.png" sign drawing' do
       it 'returns the sign drawing with the text "high_resolution" as part of the file name' do
-        expect(helper.convert_to_high_resolution(drawing_1)).to include("high_resolution")
+        expect(helper.convert_to_high_resolution(drawing_1)).to include("high_resolution.png")
+      end
+    end
+  end
+
+  describe '#stay_with_original_resolution' do
+    context 'when not provided with a "default.png" sign drawing' do
+      it 'returns the sign drawing with the text "test02-1234" as part of the file name' do
+        expect(helper.convert_to_high_resolution(drawing_2)).to include("test02-1234.png")
+      end
+    end
+  end
+
+  describe '#convert_to_high_resolution and retain the string - default' do
+    context 'when provided with a "high_resolution.png" and the string "default" as part of the file name' do
+      it 'returns the sign drawing with the text "default-1234-high_resolution" as part of the file name' do
+        expect(helper.convert_to_high_resolution(drawing_3)).to include("default-1234-high_resolution.png")
       end
     end
   end
