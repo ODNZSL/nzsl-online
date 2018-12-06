d085607ccaece531ba4a46ce459594ee9ddf4075
louise-r-blue <larblue@rocketmail.com>
2018-12-07 12:40:37 +1300

    Remove download button on print view page

--- app/assets/javascripts/vocab_sheet.js
+++ app/assets/javascripts/vocab_sheet.js
@@ -41,6 +41,7 @@ $(document).ready(function() {
       $('input.vocab_sheet_name').blur(function() { submit_vocab_sheet_name($(this)); });
 
       if (document.printView) {
+        $('img.vocab-download-button').remove()
         $('textarea').attr('readonly', true);
       }
     }
