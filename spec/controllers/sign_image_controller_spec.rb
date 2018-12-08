4ccfbab8d903cf4d77b562254d0f5dc3098f4998
louise-r-blue <larblue@rocketmail.com>
2018-12-08 19:12:32 +1300

    Fix failing tests

--- spec/controllers/sign_image_controller_spec.rb
+++ spec/controllers/sign_image_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe SignImageController, type: :controller do
-  let(:valid_params) { { height: 50, width: 40, filename: '5519/victoria_university-5519.png' } }
+  let(:valid_params) { { height: 50, width: 40, filename: '5519/victoria_university-5519-default.png' } }
 
   describe '#show' do
     context 'good request' do
@@ -13,7 +13,7 @@ RSpec.describe SignImageController, type: :controller do
     end
 
     context 'Partial request, fall back on defaults' do
-      before { get :show, params: { filename: '5519/victoria_university-5519.png' } }
+      before { get :show, params: { filename: '5519/victoria_university-5519-default.png' } }
 
       it { expect(response).to have_http_status(:ok) }
     end
@@ -34,7 +34,7 @@ RSpec.describe SignImageController, type: :controller do
       before { get :show, params: valid_params }
 
       it 'returns an image with those dimensions' do
-        expect(assigns(:local_filename)).to include('40x50-5519-victoria_university-5519.png')
+        expect(assigns(:local_filename)).to include('40x50-5519-victoria_university-5519-default.png')
       end
     end
   end
