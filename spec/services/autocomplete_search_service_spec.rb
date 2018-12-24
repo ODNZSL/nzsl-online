db8c5bfbafbed2c65ba44d938b96ac5b9f0f00ef
Eoin Kelly <eoin@rabidtech.co.nz>
2018-12-24 17:14:50 +1300

    Refactor autocomplete search and improve error handling & logging
    * Pull autocomplete searching out into a service object
    * Use Faraday instead of built-in `Net::HTTP` because `Faraday` is much more
      disciplined about the exceptions it will raise (they are all under
      `Faraday::Error`)
    * Log a warning and return an empty result-set if anything goes wrong while
      doing the autocomplete search
    * Log autocomplete requests in development (handy for debugging)

---
+++ spec/services/autocomplete_search_service_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe AutocompleteSearchService do
+  describe "#find_suggestions" do
+    context "When the external services behaves as expected" do
+      let(:search_term) { "Anything" }
+      let(:expected_suggestions) { %w{1 2 3 4 5 6 7 8 9 10} }
+      let(:stubbed_faraday_connection) do
+        Faraday::Connection.new do |faraday|
+          faraday.use Faraday::Adapter::Test do |stub|
+            stub.get '/' do
+              [200, {'Content-Type' => 'text/plain'}, expected_suggestions.join("\n")]
+            end
+          end
+        end
+      end
+
+      subject { AutocompleteSearchService.new(search_term: search_term, faraday_connection: stubbed_faraday_connection) }
+
+      it "Returns the expected suggestions" do
+        results = subject.find_suggestions
+        expect(results).to eq(expected_suggestions)
+      end
+    end
+
+    context "When there is an error communicating with the external search service" do
+      let(:search_term) { "Anything" }
+      let(:expected_suggestions) { %w{1 2 3 4 5 6 7 8 9 10} }
+      let(:stubbed_faraday_connection) do
+        Faraday::Connection.new do |faraday|
+          faraday.use Faraday::Adapter::Test do |stub|
+            stub.get '/' do
+              fail(Faraday::Error, "Simulated autocomplete service failure")
+            end
+          end
+        end
+      end
+      let(:log_accumulator) { "" }
+      let(:logger) do
+        Logger.new(StringIO.new(log_accumulator))
+      end
+
+      subject { AutocompleteSearchService.new(search_term: search_term, faraday_connection: stubbed_faraday_connection, logger: logger) }
+
+      it "Recovers from the error by returning an empty result-set" do
+        results = subject.find_suggestions
+        expect(results).to eq([])
+      end
+
+      it "Logs an explanatory message" do
+        subject.find_suggestions
+        expect(log_accumulator).to include("Recovered from failed autocomplete search")
+      end
+    end
+
+    context "When it receives more than the requested number of results" do
+      let(:search_term) { "Anything" }
+      let(:all_suggestions) { %w{1 2 3 4 5 6 7 8 9 10 11 12} }
+      let(:expected_suggestions) { %w{1 2 3 4 5 6 7 8 9 10} }
+      let(:stubbed_faraday_connection) do
+        Faraday::Connection.new do |faraday|
+          faraday.use Faraday::Adapter::Test do |stub|
+            stub.get '/' do
+              [200, {'Content-Type' => 'text/plain'}, all_suggestions.join("\n")]
+            end
+          end
+        end
+      end
+
+      subject { AutocompleteSearchService.new(search_term: search_term, faraday_connection: stubbed_faraday_connection) }
+
+      it "Discards extra results" do
+        results = subject.find_suggestions
+        expect(results).to eq(expected_suggestions)
+      end
+    end
+  end
+end
+
