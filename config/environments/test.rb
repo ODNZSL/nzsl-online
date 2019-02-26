425c5466cd9d01418b2a19949216c2446f7d0994
Eoin Kelly <eoin@rabidtech.co.nz>
2019-02-26 17:22:25 +1300

    Add small in-memory cache for tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
 
+  # We want to test caching so give ourselves a little in-memory cache to use
+  # for tests
+  config.cache_store = :memory_store, { size: 5.megabytes }
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
