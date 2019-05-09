b50cf86f4b953dafa1e6b0cd831b23199b15da0e
Brenda <brenda@wallace.net.nz>
2019-05-10 11:53:52 +1200

    Hard code which sign we view in percy_spec

--- spec/features/percy/percy_spec.rb
+++ spec/features/percy/percy_spec.rb
@@ -8,17 +8,17 @@ describe 'Test with visual testing', type: :feature, js: true do
 
   before { SeedDataService.load_all }
 
-  describe 'home' do
-    it 'loads homepage' do
-      visit root_path
-      Percy.snapshot(page, name: "homepage")
-    end
-  end
-
-  # describe 'signs' do
-  #   it 'loads signs#show' do
-  #     visit sign_path(sign)
-  #     Percy.snapshot(page, name: "signs#show")
+  # describe 'home' do
+  #   it 'loads homepage' do
+  #     visit root_path
+  #     Percy.snapshot(page, name: "homepage")
   #   end
   # end
+
+  describe 'signs' do
+    it 'loads signs#show' do
+      visit sign_path(id: '1301')
+      Percy.snapshot(page, name: "signs#show")
+    end
+  end
 end
