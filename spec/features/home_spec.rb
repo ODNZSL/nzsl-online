cff0888f0660c1e338378cb8645aa6fc0d0c4201
Brenda <brenda@wallace.net.nz>
2019-05-10 15:19:28 +1200

    Home page feature spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -9,7 +9,7 @@ describe 'Test with visual testing', type: :feature, js: true do
   before { SeedDataService.load_all }
   before { allow Sign.to receive(:random).and_return Sign.first(id: sign_id) }
   describe 'home' do
-    xit 'root_path' do
+    it 'root_path' do
       # Make sure we have the same random sign,
       # so we don't get visual differences every run
       visit root_path
