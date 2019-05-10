913aaefdcd794036c118929823a211b86b625a50
Brenda <brenda@wallace.net.nz>
2019-05-10 15:19:38 +1200

    rubocop lint

--- spec/features/signs_spec.rb
+++ spec/features/signs_spec.rb
@@ -9,15 +9,15 @@ describe 'Test with visual testing', type: :feature, js: true do
   describe 'signs' do
     describe 'signs#show' do
       before { visit sign_path(id: sign_id) }
-      it { expect(page).to have_text 'goodbye, hello'}
-      after { Percy.snapshot(page, name: "signs#show") }
+      it { expect(page).to have_text 'goodbye, hello' }
+      after { Percy.snapshot(page, name: 'signs#show') }
     end
     it 'signs#search' do
       visit root_path
       fill_in :s, with: 'hello'
       click_button class: 'search-button'
       expect(page).to have_link 'goodbye, hello'
-      Percy.snapshot(page, name: "signs#search")
+      Percy.snapshot(page, name: 'signs#search')
     end
   end
 end
