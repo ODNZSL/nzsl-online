5c58a5759c5d45900d53e92df39577cf8a30d519
Eoin Kelly <eoin@rabidtech.co.nz>
2019-09-23 14:38:06 +1200

    Build handshapes advanced search without mutating list of handshapes
    Array#shift will change the underlying array so we use a slice instead.

--- app/models/sign_menu.rb
+++ app/models/sign_menu.rb
@@ -53,7 +53,7 @@ class SignMenu # rubocop:disable Metrics/ClassLength
     ['3.3.Head', '3.4.Top of Head', '3.5.Eyes', '3.6.Nose', '3.7.Ear', '3.8.Cheek', '3.9.Lower Head'].freeze,
     ['4.0.Body', '4.10.Neck/Throat', '4.11.Shoulders', '4.12.Chest', '4.13.Abdomen', '4.14.Hips/Pelvis/Groin', '4.15.Upper Leg'].freeze, # rubocop:disable Metrics/LineLength
     ['5.0.Arm', '5.16.Upper Arm', '5.17.Elbow', '5.18.Lower Arm'].freeze,
-    ['6.0.Hand', '6.19.Wrist', '6.20.Fingers/Thumb', '6.21.Palm of Hand', '6.22.Back of Hand', '6.23.Blades of Hand'].freeze
+    ['6.0.Hand', '6.19.Wrist', '6.20.Fingers/Thumb', '6.21.Palm of Hand', '6.22.Back of Hand', '6.23.Blades of Hand'].freeze # rubocop:disable Metrics/LineLength
   ].freeze
 
   LOCATION_GROUPS = [
