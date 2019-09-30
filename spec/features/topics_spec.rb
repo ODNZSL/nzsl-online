6c49177b53285242cde0fb3b7a17c521be79ff47
Eoin Kelly <eoin@rabidtech.co.nz>
2019-09-30 21:06:53 +1300

    Fix bug preventing pagination of search results
    * Fix bug in `Sign.paginate` where the count of total results was omitted
      preventing pagination links from displaying.
    * Add a test verifying that pagination works

---
+++ spec/features/topics_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Topics', js: true do
+  let(:sign_id) { '1301' }
+
+  before do
+    SeedDataService.load_all
+    allow(Sign).to receive(:random).and_return Sign.first(id: sign_id)
+  end
+
+  describe 'Actions and Activities' do
+    it 'works' do
+      visit '/topics'
+      click_on 'Actions and activities'
+
+      expect(page).to have_css('.pagination_container')
+      expect(page).to have_content('previous')
+      expect(page).to have_content('next')
+    end
+  end
+end
