cb5b8ecadc78c4e2bd6f91d671893dc1d158181b
Eoin Kelly <eoin@eoinkelly.info>
2020-05-23 23:17:49 +1200

    Upgrade asset pre-compilation instructions and remove now unused assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.assets.precompile += %w(video-js.swf vjs.eot vjs.svg vjs.ttf vjs.woff)
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
