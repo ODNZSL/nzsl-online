0754cf5b15dbd01a09f5c0db5e54607e7756b730
Eoin Kelly <eoin@eoinkelly.info>
2020-05-25 07:46:50 +1200

    Run eslint --fix to fix JS formatting issues

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -4,14 +4,17 @@ CKEDITOR.editorConfig = function(config) {
   config.width = '500';
   config.height = '500';
   config.toolbar_Basic = [
-    { name: 'document',    items: [ 'Source' ] },
-    { name: 'clipboard',   items: [ 'Cut','Copy','Paste','PasteText','PasteFromWord' ] },
-    { name: 'edit',        items: [ 'Undo','Redo' ] },
-    { name: 'styles',      items: [ 'Format'] },
+    {name: 'document', items: ['Source']},
+    {name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord']},
+    {name: 'edit', items: ['Undo', 'Redo']},
+    {name: 'styles', items: ['Format']},
     '/',
-    { name: 'basicstyles', items: [ 'Bold','Italic','-', 'NumberedList','BulletedList','-','Outdent','Indent','Blockquote'] },
-    { name: 'links',       items: [ 'Link','Unlink','Anchor' ] },
-    { name: 'insert',      items: [ 'Image','Table', '-', 'HorizontalRule', '-', 'SpecialChar' ] },
+    {
+      name: 'basicstyles',
+      items: ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'],
+    },
+    {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
+    {name: 'insert', items: ['Image', 'Table', '-', 'HorizontalRule', '-', 'SpecialChar']},
   ];
   config.toolbar = 'Basic';
 };
