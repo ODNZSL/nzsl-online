65c3917bbd03da5cac11743d589c64b0448ae148
Brenda Wallace <brenda@brenda.nz>
2020-07-11 15:32:25 +1200

    Rubocop fixes

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -83,15 +83,13 @@ module SearchHelper # rubocop:disable Metrics/ModuleLength
 
   def tab_selected?(classes)
     keys = @query.select { |_key, value| value.present? }.keys # rubocop:disable Rails/HelperInstanceVariable
-    selected = if %w(tag usage).any? { |key| keys.include?(key) } || (keys.include?('s') && keys.length > 1)
-                 classes.include?(:advanced)
-               elsif %w(hs l lg).any? { |key| keys.include?(key) }
-                 classes.include?(:signs)
-               else
-                 classes.include?(:keywords)
+    if %w(tag usage).any? { |key| keys.include?(key) } || (keys.include?('s') && keys.length > 1)
+      classes.include?(:advanced)
+    elsif %w(hs l lg).any? { |key| keys.include?(key) }
+      classes.include?(:signs)
+    else
+      classes.include?(:keywords)
                end
-
-    selected
   end
 
   def display_locations_search_term(simple = false)
