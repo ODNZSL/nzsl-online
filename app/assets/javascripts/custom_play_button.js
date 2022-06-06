33b5a2c0dbba5845c586308ebedef6365321995c
Josh McArthur <joshua.mcarthur@gmail.com>
2022-06-07 09:58:38 +1200

    Use native playback speed control for videos (#1463)
    * Update playback speed controls to use native playback control, rather than switching to an alternative video source
    
    This reduces asset load size, and also means that the dictionary does not need to maintain separate slow-motion videos.
    
    * Refactor custom_play_button to trigger behaviour based on play/pause events so that external scripts can use native APIs to play and pause video
    
    * Adjust playback rate for slow-mo after testing to 0.5

--- app/assets/javascripts/custom_play_button.js
+++ app/assets/javascripts/custom_play_button.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var hasPlayButton = true;
+
   adjustVideoControlsToScreenSize();
 
   $(window).resize(function() {
@@ -10,6 +12,12 @@ $(document).ready(function() {
       e.preventDefault();
       videoResponse(this);
     }
+  }).on('play', function(evt) {
+    pauseOtherVideos(evt.target);
+    hasPlayButton && $(evt.target)
+        .closest('.video-container')
+        .children('.play-button')
+        .css('opacity', '0');
   });
 
   $('.play-button').click(function(e) {
@@ -19,47 +27,26 @@ $(document).ready(function() {
 
   function adjustVideoControlsToScreenSize() {
     if (Modernizr.touch && !Foundation.MediaQuery.atLeast('large')) {
+      hasPlayButton = false;
       $('.play-button').hide();
       $('video').each(function() {
         $(this).prop('controls', true);
         $(this).prop('controlsList', 'nodownload');
       });
     } else {
+      hasPlayButton = true;
       $('.play-button').show();
       $(this).prop('controls', false);
     }
   }
 
   function videoResponse(video) {
-    video.paused == true ? playVideo(video) : pauseVideo(video);
-  }
-
-  function playVideo(video) {
-    pauseOtherVideos(video);
-    $(video)
-        .closest('.video-container')
-        .children('.play-button')
-        .css('opacity', '0');
-    $(video)
-        .get(0)
-        .play();
-  }
-
-  function pauseVideo(video) {
-    $(video)
-        .closest('.video-container')
-        .children('.play-button')
-        .css('opacity', '0');
-    $(video)
-        .get(0)
-        .pause();
+    video.paused ? video.play() : video.pause();
   }
 
   function pauseOtherVideos(currentVideo) {
-    $('video').each(function() {
-      if (this != currentVideo) {
-        pauseVideo(this);
-      }
+    $('video').not(currentVideo).each(function() {
+      this.pause();
     });
   }
 });
