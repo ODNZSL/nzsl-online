84209ebaf86fb8986077b9c84bd142f39fda7250
Josh McArthur <joshua.mcarthur@gmail.com>
2022-11-30 11:04:11 +1300

    Implement gloss autolinking using regexp matching

--- app/helpers/sign_examples_helper.rb
+++ app/helpers/sign_examples_helper.rb
@@ -1,18 +1,13 @@
 module SignExamplesHelper
-  def render_transcription(transcription, sign_id)
-    transcription_links = transcription.map do |sign|
-      if sign.is_a?(String)
-        sign
-      elsif sign[:id] == sign_id
-        tag.strong sign[:gloss]
-      else
-        link_to h(sign[:gloss]),
-                sign_url(sign[:id]),
-                class: 'js-ga-link-submission',
-                onclick: "_gaq.push(['_trackEvent', 'Sign', 'Click', 'example #{sign[:id]}']);"
-      end
-    end
+  def autolink_glosses(sentence, current_sign_id: nil)
+    sanitize(sentence.gsub(/(?<gloss>[^\d\s]+)\[(?<id>\d+)\]/) do
+      sign = Regexp.last_match.named_captures
+      next tag.strong(sign['gloss']) if sign['id'] == current_sign_id
 
-    safe_join(transcription_links, ' ')
+      link_to sign['gloss'],
+              sign_url(sign['id']),
+              class: 'js-ga-link-submission',
+              onclick: "_gaq.push(['_trackEvent', 'Sign', 'Click', 'example #{sign[:id]}']);"
+    end)
   end
 end
