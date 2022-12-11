99492616ab77f062c9714857eaa1bb095ffa6c80
Josh McArthur <joshua.mcarthur@gmail.com>
2022-12-12 10:58:17 +1300

    Implement Signbank::Record#== to enable RSpec matchers like 'eq' to work without a primary key

--- app/models/signbank/record.rb
+++ app/models/signbank/record.rb
@@ -5,5 +5,16 @@ module Signbank
     # Signbank data should never be mutated, but when we run tests we need
     # to create test records
     after_initialize { readonly! unless Rails.env.test? }
+
+    ##
+    # Useful for testing - related Signbank records don't have primary keys,
+    # so in this case we consider them equal if they are the same type and
+    # have the same attributes
+    def ==(other)
+      return super if self.class.primary_key.present?
+
+      other.instance_of?(self.class) &&
+        other.attributes == attributes
+    end
   end
 end
