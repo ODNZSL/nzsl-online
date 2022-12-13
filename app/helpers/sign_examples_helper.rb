2dfbc73b98cc14c62136be697894d78f6b5d136d
Josh McArthur <joshua.mcarthur@gmail.com>
2022-12-14 12:39:29 +1300

    Strip carets from example sentence notation

--- app/helpers/sign_examples_helper.rb
+++ app/helpers/sign_examples_helper.rb
@@ -1,6 +1,13 @@
 module SignExamplesHelper
   def autolink_glosses(sentence, current_sign_id: nil)
-    sanitize(sentence.gsub(/(?<gloss>[^\d\s]+)\[(?<id>\d+)\]/) do
+    # Sentences have two notation styles that we handle:
+    #   * Leading caret is used as an 'escape sequence'. We just strip these, they
+    #     are mostly left over from Freelex - e.g. ^hs:, ^fp:. They don't get formatted otherwise.
+    #   * Gloss links are notated by non-numeric and non-space characters, followed by
+    #     square brackets containing a numeric ID. We turn these into links, unless
+    #     it's the current sign, in which case it is not linked, but we display it
+    #     as bold.
+    sanitize(sentence.tr("^", "").gsub(/(?<gloss>[^\d\s]+)\[(?<id>\d+)\]/) do
       sign = Regexp.last_match.named_captures
       next tag.strong(sign['gloss']) if sign['id'] == current_sign_id
 
