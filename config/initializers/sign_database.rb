394df2e411368b624aa718aee5c228f75c41fe5b
Josh McArthur <joshua.mcarthur@gmail.com>
2022-12-21 11:25:35 +1300

    Remove guard clause, since tests require a Signbank database regardless of mode

--- config/initializers/sign_database.rb
+++ config/initializers/sign_database.rb
@@ -1,8 +1,6 @@
-# Conditionally switch between dictionary sign models based on an environment variable.
-# This allows us to quickly switch between searching Freelex and Signbank data for signs.
-return if SignModel.freelex?
-
 # Update the dictionary file if it is older than 1 month
+# We update this file in both dictionary modes because our tests
+# expect the database to test across both modes
 path = Rails.root.join('db', 'dictionary.sqlite3')
 Rails.application.load_tasks
 Rake::Task['dictionary:update'].execute if !path.exist? || path.mtime <= 1.month.ago
