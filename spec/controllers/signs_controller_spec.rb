16a9aa5f038b03128195824d33b6b57e86dbc4d2
Cameron Hopkinson <hopkincame@MSI>
2023-02-16 13:43:50 +1300

    Removed requests page and moved sign controller to top level

--- spec/controllers/signs_controller_spec.rb
+++ spec/controllers/signs_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Signbank::SignsController, type: :controller do
+RSpec.describe SignsController, type: :controller do
   describe '/' do
     before { Rails.application.load_seed }
     render_views
