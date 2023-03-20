62ea326cb5bc2f7915772cef979c041298079964
Josh McArthur <joshua.mcarthur@gmail.com>
2023-03-21 11:00:26 +1300

    NZSL-138: Add a polyfill for cross-origin downloads by downloading as a Blob to link to

---
+++ app/assets/javascripts/download-link.js
@@ -0,0 +1,43 @@
+/**
+ * This function 'polyfills' cross-origin file downloads. It does so by fetching
+ * the file as a blob, then creating a a[download] with the blob URL and
+ * clicking it, then removing it from the DOM.
+ *
+ * Since we have a CORS policy allowing fetches to our media bucket, this works
+ * cross-origin, since after fetching, the blob is same-origin.
+ *
+ * It derives the filename to use as the basename of the href URL. This assumes
+ * that the URL has a filename in it, rather than just a path name. If the
+ * basename cannot be derived, 'file' is used as the filename.
+ */
+function initializeDownloadLinks() {
+  $(document).on('click', '.download-link', function(evt) {
+    evt.preventDefault();
+    var link = this;
+    var path_parts = new URL(link.href).pathname.split('/');
+    var filename = path_parts[path_parts.length - 1] || 'file';
+
+    fetch(link.href)
+        .then(function(response) {
+          return response.blob();
+        })
+        .then(function(blob) {
+          var a = document.createElement('a');
+          a.download = filename;
+          a.style.display = 'none';
+          a.href = URL.createObjectURL(blob);
+          a.addEventListener('click', function() {
+            setTimeout(function() {
+              URL.revokeObjectURL(a.href);
+              a.remove();
+            }, 1000);
+          });
+          document.body.append(a);
+          a.click();
+        });
+  });
+}
+
+$(function() {
+  initializeDownloadLinks();
+});
