f5605b1cb9c6ed37c31cdc3ee73f74d3ac9c18dc
Jacob Briggs <jacob.briggs@ackama.com>
2026-01-29 14:18:49 +1300

    Add in a work around for ckeditor no supporting rails 8 yet

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-#require 'ckeditor-rails/engine'
+require 'ckeditor-rails/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
