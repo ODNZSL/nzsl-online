57506b6dd64797f46289e0bceebe60c32b27c482
Laura Corkin <57056459+nzlaura@users.noreply.github.com>
2026-07-03 09:35:45 +1200

    fix: set reply to on feedback mailer (#1672)

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -4,6 +4,6 @@ class FeedbackMailer < ApplicationMailer
   def email(model)
     @feedback = model
     attachments[@feedback.video_file_name] = File.read(@feedback.video.path) if @feedback.video_file_name
-    mail(to: CONTACT_EMAIL, subject: 'NZSL Website Feedback')
+    mail(to: CONTACT_EMAIL, subject: 'NZSL Website Feedback', reply_to: @feedback.email)
   end
 end
