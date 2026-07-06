14c90fe3e9be7fd388cb1539d283cd5888718ef2
nzlaura <nzlaura.williams@gmail.com>
2026-07-06 15:24:46 +1200

    fix: add cloudflares test site/secret keys so rspec can run in ci

--- config/initializers/cloudflare_turnstile.rb
+++ config/initializers/cloudflare_turnstile.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 RailsCloudflareTurnstile.configure do |config|
-  config.site_key = ENV['TURNSTILE_SITE_KEY']
-  config.secret_key = ENV['TURNSTILE_SECRET_KEY']
+  config.site_key = ENV.fetch('TURNSTILE_SITE_KEY', '1x00000000000000000000AA')
+  config.secret_key = ENV.fetch('TURNSTILE_SECRET_KEY', '1x0000000000000000000000000000000AA')
 end
