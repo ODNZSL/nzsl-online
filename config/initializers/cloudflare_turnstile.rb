d285cac379cd27142efd71c1402670e1084fed3d
Laura Corkin <57056459+nzlaura@users.noreply.github.com>
2026-07-08 14:08:01 +1200

    fix: form returning 500 errors with turnstile enabled (#1675)
    * fix: form returning 500 errors with cloudflare enabled
    
    * fix: failing test?
    
    * fix: make sure it's only enabled in production?
    
    * fix: reverted one change too many

--- config/initializers/cloudflare_turnstile.rb
+++ config/initializers/cloudflare_turnstile.rb
@@ -4,4 +4,5 @@ RailsCloudflareTurnstile.configure do |config|
   # fallback values are cloudflare test keys: https://developers.cloudflare.com/turnstile/troubleshooting/testing/
   config.site_key = ENV.fetch('TURNSTILE_SITE_KEY', '2x00000000000000000000AB')
   config.secret_key = ENV.fetch('TURNSTILE_SECRET_KEY', '2x0000000000000000000000000000000AA')
+  config.enabled = Rails.env.production?
 end
