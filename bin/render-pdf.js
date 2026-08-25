2ff8767ab39e0e12da06bfa29cca4233062d2a2f
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-08-25 13:55:10 +1200

    feat: update puppeteer

--- bin/render-pdf.js
+++ bin/render-pdf.js
@@ -10,10 +10,13 @@
 
 const puppeteer = require("puppeteer");
 
-const inputPath = `file://${process.argv[2]}`;
-const outputPath = process.argv[3];
-const basic_auth_username = process.argv[4];
-const basic_auth_password = process.argv[5];
+const [
+  ,,
+  inputPath,
+  outputPath,
+  basicAuthUsername,
+  basicAuthPassword,
+] = process.argv
 
 console.log("Input path:", inputPath);
 console.log("Output path:", outputPath);
@@ -22,23 +25,35 @@ console.log("Output path:", outputPath);
 but due to issues getting it running on Heroku this option is required. */
 
 (async () => {
-  const browser = await puppeteer.launch({args: ["--no-sandbox"]});
-  const page = await browser.newPage();
-
-  if (basic_auth_username) {
-    await page.authenticate({username: basic_auth_username, password: basic_auth_password});
-  }
-
-  console.log("Starting PDF conversion");
-  // https://github.com/GoogleChrome/puppeteer/blob/v1.4.0/docs/api.md#pagegotourl-options
-  await page.goto(inputPath, { waitUntil: "load" });
-
-  // https://github.com/GoogleChrome/puppeteer/blob/v1.4.0/docs/api.md#pagepdfoptions
-  await page.pdf({
-    path: outputPath,
-    format: "A4"
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-web-security", "--disable-setuid-sandbox"],
+    headless: "new"
   });
+  try {
+    const page = await browser.newPage();
 
-  console.log("PDF conversion complete");
-  await browser.close();
-})();
+    if (basicAuthUsername) {
+      await page.authenticate({ username: basicAuthUsername, password: basicAuthPassword });
+    }
+
+    await page.setDefaultTimeout(0);
+
+    console.log("Starting PDF conversion");
+    // https://github.com/GoogleChrome/puppeteer/blob/v1.4.0/docs/api.md#pagegotourl-options
+    await page.goto(inputPath, { waitUntil: "load" });
+
+    // https://github.com/GoogleChrome/puppeteer/blob/v1.4.0/docs/api.md#pagepdfoptions
+    await page.pdf({
+      path: outputPath,
+      format: "A4"
+    });
+
+    console.log("PDF conversion complete");
+  } finally {
+    await browser.close();
+  }
+})().catch(error => {
+  console.error(error)
+
+  process.exitCode = 1
+});
